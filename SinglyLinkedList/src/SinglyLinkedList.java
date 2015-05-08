edb81edc424f099e979596df7bf4b3f20ecf6b5c
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-08 16:16:19 -0400

    Add SinglyLinkedList and SinglyLinkedListTest

---
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -0,0 +1,69 @@
+import java.util.Iterator;
+
+
+public class SinglyLinkedList<E>
+{
+	protected Entry<E> head;
+	
+	public SinglyLinkedList()
+	{
+		head = null;
+	}
+	
+	public boolean isEmpty()
+	{
+		return head == null;
+	}
+	
+	public void addToFront(E element)
+	{
+		Entry<E> newEntry = new Entry<E>();
+		newEntry.element = element;
+		newEntry.next = head;
+		head = newEntry;
+	}
+	
+	public int size() 
+	{
+		int count = 0;
+    
+		for (Entry<E> current = head; current != null; current = current.next)
+		{
+			count++;
+		}
+		return count;
+	}
+	
+	protected class SinglyLinkedListIterator implements Iterator<E> 
+	{
+	    protected Entry<E> next;
+	    
+	    protected SinglyLinkedListIterator() 
+	    {
+	      next = head;
+	    } 
+	                             
+	    public E next() 
+	    {
+	      E theElement = next.element;
+	      next = next.next;
+	      return theElement;
+	    }
+	    
+	    public boolean hasNext() 
+	    {       
+	      return next != null;
+	    } 
+	    
+	    public void remove() 
+	    { 
+	      throw new UnsupportedOperationException( ); 
+	    }
+	}
+	
+	protected class Entry<E>
+	{
+		protected E element;
+		protected Entry<E> next;
+	}
+}
---
+++ SinglyLinkedList/test/SinglyLinkedListTest.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SinglyLinkedListTest 
+{
+	protected SinglyLinkedList<String> list;
+	
+	@Before
+	public void before()
+	{
+		list = new SinglyLinkedList<String>();
+	}
+	
+	@Test
+	public void testSinglyLinkedList() 
+	{
+		assertNotNull(list);
+	}
+
+	@Test
+	public void testIsEmpty() 
+	{
+		assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testAddToFront() 
+	{
+		list.addToFront("Test");
+		assertFalse(list.isEmpty());
+	}
+	
+	@Test
+	public void testSize()
+	{
+		assertEquals(0, list.size());
+		list.addToFront("test");
+		assertEquals(1, list.size());
+	}
+}
