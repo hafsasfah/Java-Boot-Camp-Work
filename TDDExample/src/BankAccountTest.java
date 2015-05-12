b5a12b27de2c6f1f37522bcfdc0840c614e46bf8
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-12 10:23:47 -0400

    Update tests

--- TDDExample/src/BankAccountTest.java
+++ TDDExample/src/BankAccountTest.java
@@ -2,38 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class BankAccountTest {
 
 	@Test
 	public void testBankAccount() {
 		BankAccount account = new BankAccount();
 		assertNotNull( account );
+		assertEquals(0, account.getBalance(), 0);
 	}
 
 	@Test
 	public void testBankAccountDouble() {
-		BankAccount account = new BankAccount(10.0);
+		BankAccount account = new BankAccount( 10 );
 		assertNotNull( account );
-	}
-
-	@Test
-	public void testGetBalance() {
-		BankAccount account = new BankAccount();
-		assertEquals(0.0, account.getBalance(), 0.0 );
-	}
-
-	@Test
-	public void testWithdraw() {
-		BankAccount account = new BankAccount();
-		account.withdraw( 10.0 );
-		assertEquals( -10.0, account.getBalance(), 0.0 );
+		assertEquals(10, account.getBalance(), 0);
 	}
 
 	@Test
 	public void testDeposit() {
+		double deposit = 15;
 		BankAccount account = new BankAccount();
-		account.deposit( 10.0 );
-		assertEquals( 10.0, account.getBalance(), 0.0 );
+		assertEquals(0, account.getBalance(), 0);
+		account.deposit( deposit );
+		assertEquals(deposit, account.getBalance(), 0);
+	}
+
+	@Test
+	public void testWithdraw() {
+		double withdrawl = 25;
+		BankAccount account = new BankAccount(50);
+		assertEquals(50, account.getBalance(), 0);
+		account.withdraw( withdrawl );
+		assertEquals(withdrawl, account.getBalance(), 0);
 	}
 
 }
