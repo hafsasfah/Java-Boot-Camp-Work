d581af5c05c8179073f1b00477e97241e10b3301
Robert Grubbs <rgrubbs88@gmail.com>
2015-05-13 13:25:06 -0400

    add change linked file

---
+++ SinglyLinkedList/src/StacksAndQueues.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Stack;
+
+
+public class StacksAndQueues{
+	
+	public static void main(String[] args)
+	{
+		int current;
+		int numbersToGenerate;
+		
+		Stack<Integer> myStack = new Stack<Integer>();
+		Random random = new Random(100);
+		
+		for(int i = 0; i < 100; i++)
+		{
+			
+		}
+			// if number > current top, push
+			// else pop the current number off
+			// check again
+			
+	}
+	
+	public void createStack()
+	{
+
+
+	}
+}
+
