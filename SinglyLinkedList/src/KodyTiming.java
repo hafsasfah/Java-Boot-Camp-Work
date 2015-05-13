b9b1d41b37a5bd4fd2f705ff715edeaff5aa3c64
Kody Meilleur <kodymeilleur@yahoo.com>
2015-05-12 21:07:15 -0400

    Checking Numbers Working

--- SinglyLinkedList/src/KodyTiming.java
+++ SinglyLinkedList/src/KodyTiming.java
@@ -11,21 +11,19 @@ public class KodyTiming {
 		addRandomNumbersToList( "linkedList", linkedList, 1000 );
 		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 1000 );
 		
-		System.out.println(arrayList.size() + "\n");
+		//checkRandomNumbers( "ArrayList", arrayList);
 		
-		checkRandomNumbers( "ArrayList", arrayList);
-		checkRandomNumbers( "linkedList", singlyLinkedList);
-		checkRandomNumbers( "singlyLinkedList", singlyLinkedList);
-		
-		//addRandomNumbersToList( "arrayList", arrayList, 10000000 );
-		//addRandomNumbersToList( "linkedList", linkedList, 10000000 );
-		//addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 10000000 );
+		addRandomNumbersToList( "arrayList", arrayList, 10000000 );
+		addRandomNumbersToList( "linkedList", linkedList, 10000000 );
+		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 10000000 );
+
 	}
 	
 	public static void addRandomNumbersToList( String name, List<Integer> list, int numbersToAdd )
 	{
+		int total = 0;
 		Random rand = new Random();
-		
+		int permrandomnum =  rand.nextInt((100 - 0) + 1);
 		long startTime, finishTime, elapsedTime;
 		startTime = System.nanoTime();
 		
@@ -33,39 +31,18 @@ public class KodyTiming {
 		{
 			int randomnum = rand.nextInt((100 - 0) + 1);
 			list.add( randomnum );
-		}
-		
-		finishTime = System.nanoTime();
-		elapsedTime = finishTime - startTime;
-		
-		System.out.printf( "Adding %d to %s took %f millseconds\n\n", numbersToAdd, name, elapsedTime / 1000000.0 );
-
-	}
-	
-	public static void checkRandomNumbers(String name, List<Integer> list)
-	{
-		
-		//check if the list contains a specific randomly generated number at least 100 times
-		Random rand = new Random();
-		int randomnum = rand.nextInt((100 - 0) + 1);
-		
-		int totalcounter = 0;
-		long startTime, finishTime, elapsedTime;
-		startTime = System.nanoTime();		
-		for(int i = 0; i < list.size(); i++)
-		{
-			if(list.get(i)==(randomnum))
+			if(permrandomnum == randomnum)
 			{
-				totalcounter++;
+				total++;
 			}
 		}
 		
 		finishTime = System.nanoTime();
 		elapsedTime = finishTime - startTime;
 		
-		System.out.println(randomnum + " appeared " + totalcounter + " times in " + name + ".");
-		System.out.printf(name + "check took %f millseconds to run.\n", elapsedTime / 1000000.0 );
-		
-		
+		System.out.printf( "Adding %d to %s took %f millseconds\n", numbersToAdd, name, elapsedTime / 1000000.0 );
+		System.out.println(permrandomnum + " appeared " + total + " times in " + name + ".");
+
 	}
+	
 }
