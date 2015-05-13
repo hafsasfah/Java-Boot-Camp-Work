c4581f8ded22aeb6daaac292032038dffbdd21bf
Kody Meilleur <kodymeilleur@yahoo.com>
2015-05-12 20:53:47 -0400

    Checking numbers sofar2

--- SinglyLinkedList/src/KodyTiming.java
+++ SinglyLinkedList/src/KodyTiming.java
@@ -11,39 +11,60 @@ public class KodyTiming {
 		addRandomNumbersToList( "linkedList", linkedList, 1000 );
 		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 1000 );
 		
-		checkRandomNumbers( "ArrayList", arrayList, 100 );
-		checkRandomNumbers( "linkedList", singlyLinkedList, 100);
-		checkRandomNumbers( "singlyLinkedList", singlyLinkedList, 100);
+		System.out.println(arrayList.size() + "\n");
 		
-		addRandomNumbersToList( "arrayList", arrayList, 10000000 );
-		addRandomNumbersToList( "linkedList", linkedList, 10000000 );
-		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 10000000 );
+		checkRandomNumbers( "ArrayList", arrayList);
+		checkRandomNumbers( "linkedList", singlyLinkedList);
+		checkRandomNumbers( "singlyLinkedList", singlyLinkedList);
+		
+		//addRandomNumbersToList( "arrayList", arrayList, 10000000 );
+		//addRandomNumbersToList( "linkedList", linkedList, 10000000 );
+		//addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 10000000 );
 	}
 	
 	public static void addRandomNumbersToList( String name, List<Integer> list, int numbersToAdd )
 	{
-		Random random = new Random(50);
+		Random rand = new Random();
+		
 		long startTime, finishTime, elapsedTime;
 		startTime = System.nanoTime();
 		
 		for ( int count = 0; count < numbersToAdd; count++ )
 		{
-			list.add( random.nextInt() );
+			int randomnum = rand.nextInt((100 - 0) + 1);
+			list.add( randomnum );
 		}
 		
 		finishTime = System.nanoTime();
 		elapsedTime = finishTime - startTime;
 		
 		System.out.printf( "Adding %d to %s took %f millseconds\n\n", numbersToAdd, name, elapsedTime / 1000000.0 );
+
 	}
 	
-	public static void checkRandomNumbers(String name, List<Integer> list, int numcheck)
+	public static void checkRandomNumbers(String name, List<Integer> list)
 	{
-		if(list.size()>=numcheck)
+		
+		//check if the list contains a specific randomly generated number at least 100 times
+		Random rand = new Random();
+		int randomnum = rand.nextInt((100 - 0) + 1);
+		
+		int totalcounter = 0;
+		long startTime, finishTime, elapsedTime;
+		startTime = System.nanoTime();		
+		for(int i = 0; i < list.size(); i++)
 		{
-			System.out.println(name + " contains at least " + numcheck + " elements." +"\n");
+			if(list.get(i)==(randomnum))
+			{
+				totalcounter++;
+			}
 		}
 		
+		finishTime = System.nanoTime();
+		elapsedTime = finishTime - startTime;
+		
+		System.out.println(randomnum + " appeared " + totalcounter + " times in " + name + ".");
+		System.out.printf(name + "check took %f millseconds to run.\n", elapsedTime / 1000000.0 );
 		
 		
 	}
