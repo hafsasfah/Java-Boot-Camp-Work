833e0cfece1e0a0f8eb9a01cbd60ad0b703c3802
Sarah Johnston <johnston-s@live.com>
2015-05-13 14:04:15 -0400

    .

--- SinglyLinkedList/src/RacingGame/Player.java
+++ SinglyLinkedList/src/RacingGame/Player.java
@@ -1,15 +1,55 @@
 package RacingGame;
 
-public interface Player {
+public class Player implements  PlayerI {
+
 	
-	//private Static String name;
+	
+	String name;
+	int cash;
+	Car vehicle;
+	
+	@Override
+	public String getName() {
+	
+		return name;
+	}
+
+	@Override
+	public int getCash() {
+		
+		return cash;
+	}
+
+	@Override
+	public int addCash(int cash) {
+		this.cash+=cash;
+		return this.cash;
+	}
+
+	@Override
+	public void setVehicle(Car vehicle) {
+		this.vehicle= vehicle;
+	}
+
+	@Override
+	public Car getVehicle() {
+		
+		return vehicle;
+	}
+
+	@Override
+	public Car getVehicle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+;
 	
 	
-	public String getName();
-	public int getCash();
-	public int addCash(int cash);
-	public void setVehicle();
-	public Car getVehicle();
+	
+	
+	
 	
 	
 	
