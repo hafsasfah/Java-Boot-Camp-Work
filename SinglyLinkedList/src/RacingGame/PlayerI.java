832046ec2d91d12ff45495571d71c1739b11c4cc
CColon1 <sikz26300@gmail.com>
2015-05-13 14:03:07 -0400

    CarClass

--- SinglyLinkedList/src/RacingGame/PlayerI.java
+++ SinglyLinkedList/src/RacingGame/PlayerI.java
@@ -8,7 +8,7 @@ public interface PlayerI {
 	public String getName();
 	public int getCash();
 	public int addCash(int cash);
-	public void setVehicle();
+	public void setVehicle(Car vehicle);
 	public Car getVehicle();
 	
 	
