eaada2f71ea5429064880edb580caaa6dff99950
asjerniga <asjerniga@gmail.com>
2015-05-13 14:28:12 -0400

    bet class added

---
+++ Group3/src/Bet.java
@@ -0,0 +1,62 @@
+
+public class Bet implements BetInterface
+{
+
+	private PlayerInterface player1; 
+	private PlayerInterface player2;
+	private int wager;
+	
+	public Bet(PlayerInterface player1, PlayerInterface player2, int wager)
+	{
+		
+		this.player1 = player1;
+		this.player2 = player2;
+		
+		this.wager = wager;
+	
+	}
+	
+	public String race()
+	{
+		
+		if(player1.getVehicle().getQuarterMileTime() > player2.getVehicle().getQuarterMileTime())
+		{
+			
+			
+			player1.addCash(wager);
+			player2.addCash(wager);
+			return player1.getName() + "in a" + player1.getVehicle().toString()
+					+  " beats " + player2.getName() + "in a" + player2.getVehicle().toString();
+			
+		}else 
+		{
+			
+			player1.addCash(wager * -1);
+			player2.addCash(wager);
+			return player1.getName() + "in a " + player1.getVehicle().toString() + "loses to " 
+			+ player2.getName() + " in a " + player2.getVehicle().toString();
+			
+			
+			
+			
+		}
+	
+		
+		
+		
+		
+	}
+	
+	
+	
+	@Override
+	public String Race() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+
+}
