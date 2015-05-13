504a1225afbc32359d314757f7fd4adfde909b0f
Kody Meilleur <kodymeilleur@yahoo.com>
2015-05-13 14:23:54 -0400

    Merge Fix

--- Group3/src/CarInterface.java
+++ Group3/src/CarInterface.java
@@ -1,19 +1,10 @@
 import java.util.Random;
 
-
-<<<<<<< HEAD
 public interface CarInterface 
-{
-=======
-public interface CarInterface {
+	{
 	String getmake();
 	String getmodel();
 	String toString();
 	double getQuarterMileTime();
-	
-	
-	
-	
->>>>>>> origin/Group3
-	
-}
+
+	}
