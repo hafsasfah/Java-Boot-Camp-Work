f0bc870e60c0b899dd1f70bb774fba34406a36f2
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-14 17:50:57 -0400

    Adding frame and panel to Game

--- Group2/src/Game.java
+++ Group2/src/Game.java
@@ -1,10 +1,26 @@
+import java.awt.Color;
+
 import javax.swing.*;
 public class Game {
+	
+	JFrame f = new JFrame("Racing Game");
+	JPanel p = new JPanel();
+
+	
+	public Game() {
+		JButton confirm = new JButton("Confirm");
+		
+		
+		f.setVisible(true);
+		f.setSize(500, 300);	
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		p.setBackground(Color.blue);
+		
+		f.add(p);
+		
+	}	
+	
 	public static void main(String[] args) {
-		Player player1 = new Player();
-		Player player2 = new Player();
-		
-		
-		
+		new Game();
 	}
 }
