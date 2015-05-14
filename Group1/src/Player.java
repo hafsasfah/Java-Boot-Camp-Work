af84133fcf730126d3181459d9392a4bc2405c6f
CColon1 <sikz26300@gmail.com>
2015-05-14 10:59:01 -0400

    src folder

---
+++ Group1/src/Player.java
@@ -0,0 +1,65 @@
+package RacingGame;
+
+public class Player implements  PlayerI {
+
+	
+	
+	String name;
+	int cash;
+	VehicleI vehicle;
+	
+	@Override
+	public String getName() {
+	
+		return name;
+	}
+
+	@Override
+	public int getCash() {
+		
+		return cash;
+	}
+
+	@Override
+	public int addCash(int cash) {
+		this.cash+=cash;
+		return this.cash;
+	}
+	@Override
+	public int subtractCash(int cash) {
+		this.cash-=cash;
+		return this.cash;
+	}
+	
+	
+	
+	public Player(String name, int cash, VehicleI vehicle) {
+		super();
+		this.name = name;
+		this.cash = cash;
+		this.vehicle = vehicle;
+	}
+
+	@Override
+	public void setVehicle(VehicleI vehicle) {
+		this.vehicle= vehicle;
+	}
+
+	
+
+	@Override
+	public VehicleI getVehicle() {
+		return vehicle;
+	}
+
+	
+;
+	
+	
+	
+	
+	
+	
+	
+	
+}
