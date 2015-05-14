5c6bc4abd1dede5960a6c10a6a36507545925bb6
Tommiec21 <Tommiec22@gmail.com>
2015-05-14 10:25:36 -0400

    CarClass Update 3

--- Group3/src/CarClass.java
+++ Group3/src/CarClass.java
@@ -1,8 +1,41 @@
+import java.util.Random;
+
 
 public class CarClass {
 
-	public CarClass() {
-		// TODO Auto-generated constructor stub
+	private String Model;
+	public CarClass()
+	{
+		
+	}
+	
+	public static int makeRandom()
+	{
+		
+	Random rand = new Random();
+	return rand.nextInt((5 - 1) +1);
+	
+	}
+		Random random = new Random (10);
+		public String getModel() {
+			return Model;
+		}
+
+		public void setModel(String model) {
+			Model = model;
+		}
+
+		public Random getRandom() {
+			return random;
+		}
+
+		public void setRandom(Random random) {
+			this.random = random;
+		}
+		
+	
+		
+		
 	}
 
-}
+
