f8dfbe1c3472a9fc85d7ae2ff4c2dd65b63112e5
CColon1 <sikz26300@gmail.com>
2015-05-14 09:43:32 -0400

    Moved the project from SinglyLinkedList to Group1

---
+++ Group1/racingGame/Car.java
@@ -0,0 +1,35 @@
+package RacingGame;
+import java.util.*;
+public class Car implements VehicleI {
+/*
++getQuarterMileTime() : double
+- make this random < 10 seconds
+
++toString() : String*/
+	
+	private String make;
+	private String model;
+	public Car(String make, String model){
+		this.make=make;
+		this.model=model;
+	}
+
+	public String getMake() {
+		return make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public String toString(){
+		return "Make: "+ make+" Model "+ model; 
+		
+	}
+	public double getQuaterMile(){
+		Random random= new Random();
+		
+		return random.nextDouble()*10;
+	}
+	
+	
+
+}
