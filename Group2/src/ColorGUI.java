beee0b7cf0e35fdcfdb9153d4fd4d432189971dd
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-15 00:03:41 -0400

    Action listener buttons for ColorGUI

--- Group2/src/ColorGUI.java
+++ Group2/src/ColorGUI.java
@@ -1,13 +1,16 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 
+
 public class ColorGUI extends JPanel {
-	public final String red="Red";
-	public final String white="White    ";
-	public final String black="Black";
-	public final String green="Green";
+	public final String red = "Red          ";
+	public final String white = "White";
+	public final String black = "Black";
+	public final String green = "Green";
 	
 	// Making selection Buttons
 	private JRadioButton Red; 
@@ -16,32 +19,44 @@ public class ColorGUI extends JPanel {
 	private JRadioButton Green;
 	private JRadioButton button;
 
+	private class exitButtonListener implements ActionListener {
+	      public void actionPerformed(ActionEvent e) {
+	          System.exit(0);
+	      }
+	}
+	
+	private class confirmButtonListener implements ActionListener {
+		Vehicle player = new Vehicle(null, black);
+		public void actionPerformed(ActionEvent e) {
+			if (Red.isSelected()) {
+				player.setColor(red);
+				//System.out.println("Red is selected");
+			}
+			
+			else if (White.isSelected()) {
+				player.setColor(white);
+			}
+			
+			else if (Black.isSelected()) {
+				player.setColor(black);
+			}
+			
+			else if (Green.isSelected()) {
+				player.setColor(green);
+			}
+	    }
+	}
 	
 	public ColorGUI(){
-		Vehicle player=new Vehicle(null, black);
+		
 		setLayout(new GridLayout(4,1));
 		
 		//Creating Radio buttons
-		Red= new JRadioButton(red);
-		if(Red.isSelected())
-		{
-		 player.setColor(red);
-		}
-		White= new JRadioButton(white);
-		if(White.isSelected())
-		{
-			player.setColor(white);
-		}
-		Black=new JRadioButton(black);
-		if(Black.isSelected())
-		{
-			player.setColor(black);
-		}
+		Red = new JRadioButton(red);
+		White = new JRadioButton(white);
+		Black = new JRadioButton(black);
 		Green=new JRadioButton(green);
-		if(Green.isSelected())
-		{
-			player.setColor(green);
-		}
+		
 		
 		// Group the radio buttons
 		button=new JRadioButton();
@@ -49,6 +64,18 @@ public class ColorGUI extends JPanel {
 		button.add(White);
 		button.add(Black);
 		button.add(Green);
+		
+		JPanel buttonPanel = new JPanel();
+		JButton exit = new JButton("Exit");
+		JButton confirm = new JButton("Confirm");
+
+
+      // Register the action listeners.
+        confirm.addActionListener(new confirmButtonListener());
+        exit.addActionListener(new exitButtonListener());
+		
+        buttonPanel.add(confirm);
+        buttonPanel.add(exit);
 		
 		setBorder(BorderFactory.createTitledBorder("Choose a Color   "));
 		
