c22e956bc85b8382cb049ab340a45d98efc81071
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-15 09:25:37 -0400

    Buttons show up on Gui.java

--- Group2/src/Gui.java
+++ Group2/src/Gui.java
@@ -3,64 +3,63 @@ import java.awt.BorderLayout;
 import javax.swing.*;
 
 public class Gui extends JFrame {
-	
-	
-		   private VehicleGui vehicle;   
-		   private Player player1;
-		   private Player player2;
-		   private JPanel buttonPanel;
-		   private BetGUI bet;
-		   private ColorGUI color;
-		   private JButton confirm;
-		   private JButton exit;
-		//   private Vehicle make=new Vehicle();
-		//   private Player wager=new Player();
 
-		 
-		   public Gui()
-		   {		   
-			   setTitle("Racing Game");    
-		      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		      setLayout(new BorderLayout());  	
-		      
-		      vehicle=new VehicleGui();
-		      bet=new BetGUI();
-		      color=new ColorGUI();
-		      
-		      buildButtonPanel();
-		      
-		      add(vehicle, BorderLayout.CENTER);
-		      add(color,BorderLayout.EAST);
-		     // add(bet,BorderLayout.EAST);
-		      add(buttonPanel, BorderLayout.SOUTH);
-		     
 
-		     
-		      pack();
-		      setVisible(true);
-		   }
+	private VehicleGui vehicle;   
+	private Player player1;
+	private Player player2;
+	private JPanel buttonPanel;
+	private BetGUI bet;
+	private ColorGUI color;
+	private JButton confirm;
+	private JButton exit;
+//   private Vehicle make=new Vehicle();
+//   private Player wager=new Player();
+
+
+	public Gui() {	  
+		setTitle("Racing Game");    
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     	setLayout(new BorderLayout());  	
+     
+     	vehicle=new VehicleGui();
+     	bet=new BetGUI();
+     	color=new ColorGUI();
+     
+     	buildButtonPanel();
+     
+     	add(vehicle, BorderLayout.CENTER);
+     	add(color,BorderLayout.EAST);
+     	// add(bet,BorderLayout.EAST);
+     	add(buttonPanel, BorderLayout.SOUTH);
+    
+
+    
+     	pack();
+     	setVisible(true);
+	}
 
 	private void buildButtonPanel() {
-		buttonPanel=new JPanel();
-		confirm=new JButton();
-		exit= new JButton();
-		
-		// confirm.addActionListener(new confirmButtonListener());
-	     //   exit.addActionListener(new exitButtonListener());
-			
-	        buttonPanel.add(confirm);
-	        buttonPanel.add(exit);
-					
-		}
-	
-	//private class nconfirmButtonListener implements ActionListener{
-		
-	//}
+			buttonPanel=new JPanel();
+			confirm=new JButton("Confirm");
+			exit= new JButton("Exit");
 
-	public static void main(String[] agrs){
-	
-			new Gui();
-		}
+// confirm.addActionListener(new confirmButtonListener());
+    //   exit.addActionListener(new exitButtonListener());
+
+       buttonPanel.add(confirm);
+       buttonPanel.add(exit);
+
+}
+
+//private class nconfirmButtonListener implements ActionListener{
+
+//}
+
+public static void main(String[] agrs){
+
+	new Gui();
+}
 
 
 }
