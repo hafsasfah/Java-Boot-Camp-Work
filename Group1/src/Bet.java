642dbefe8e8224f8186a95002f958574736ff3fe
CColon1 <sikz26300@gmail.com>
2015-05-15 08:22:38 -0400

    fixed if statments and chagned print statements  to return type;

--- Group1/src/Bet.java
+++ Group1/src/Bet.java
@@ -1,11 +1,11 @@
 
 public class Bet {
 	
-	private Player player1;
-	private Player player2;
+	private PlayerI player1;
+	private PlayerI player2;
 	private int wager = 0;
 
-	public Bet( Player player1, Player  player2, int wager)
+	public Bet( PlayerI player1, PlayerI  player2, int wager)
 	{
 		this.player1 = player1;
 		this.player2 = player1;
@@ -13,23 +13,26 @@ public class Bet {
 	}
 	
 	public String Race()
-	{	
+	{	//TODO: what are these if statements doing!?!? Im assuming you want to compare between the two speeds
 		
-		if player1.getVehicle().getQuarterMile(); //TODO: these should be player1.getVehicle().getQuaterMile() ;
+		double player1Speed=player1.getVehicle().getQuarterMile(); ;
+		double player2Speed=player2.getVehicle().getQuarterMile();
+		
+		if (player1Speed<player2Speed)
 		{
 			player1.addCash(wager);
 			player2.addCash(-(wager));
-			System.out.print("Player 1 Wins");
+			return"Player 1 Wins";
 		}
-		else if player2.getVehicle().getQuarterMile();
+		else if (player2Speed<player1Speed)
 		{
 			player1.addCash(-(wager));
 			player2.addCash(wager);
-			System.out.print("Player 2 Wins");
+			return"Player 2 Wins";
 		}
 		else
 		{
-			System.out.print("There is a tie");
+			return "There is a tie";
 		}
 
 	}
