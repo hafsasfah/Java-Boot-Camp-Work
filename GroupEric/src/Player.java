b91136386e925e97c235aa3afeca33570b7fa45e
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-14 22:32:50 -0400

    Update player to be able to setup as a panel

--- GroupEric/src/Player.java
+++ GroupEric/src/Player.java
@@ -1,21 +1,52 @@
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-
-public class Player implements iPlayer {
-	
+public class Player implements iPlayer 
+{
+	private boolean isValid;
 	private String name;
 	private int cash;
 	private iVehicle vehicle;
-
-	public Player( String name, int cash )
-	{
-		this.name = name;
-		this.cash = cash;
-	}
+	private JLabel enterNameLabel;
+	private JTextField nameTextField;
+	private JButton createPlayerButton;
+	private JPanel panel;
+	private JLabel nameLabel;
+	private JLabel cashLabel;
 	
+	public Player( int cash )
+	{
+		isValid = false;
+		this.cash = cash;
+		
+		panel = new JPanel();
+		panel.setLayout( new GridLayout(4,1));
+		enterNameLabel = new JLabel("Enter the player's name");
+		nameTextField = new JTextField(10);
+		
+		createPlayerButton = new JButton("Create Player");
+		createPlayerButton.addActionListener( new PlayerSetupActionListener() );
+		
+		JPanel topPanel = new JPanel();
+		topPanel.add(enterNameLabel);
+		panel.add(topPanel);
+		
+		JPanel middlePanel = new JPanel();
+		middlePanel.add(nameTextField);
+		panel.add(middlePanel);
+		
+		vehicle = new Car();
+		panel.add(vehicle.getPanel());
+		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.add(createPlayerButton);
+		panel.add(bottomPanel);
+		
+		nameLabel = new JLabel();
+		cashLabel = new JLabel();
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -25,7 +56,8 @@ public class Player implements iPlayer {
 	}
 
 	public void addCash(int cash ) {
-		cash += cash;
+		this.cash += cash;
+		updateCashLabel();
 	}
 
 	public void setVehicle(iVehicle vehicle) {
@@ -36,15 +68,48 @@ public class Player implements iPlayer {
 		return vehicle;
 	}
 
-	public JPanel getPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(3,1));
-		JLabel nameLabel = new JLabel(name);
-		JLabel cashLabel = new JLabel("Cash: " + cash);
-		panel.add(nameLabel);
-		panel.add(cashLabel);
-		panel.add(vehicle.getPanel());
+	public JPanel getPanel()
+	{
 		return panel;
 	}
-
+	
+	public boolean isReady()
+	{
+		return isValid;
+	}
+	
+	private void updatePanel() 
+	{
+		nameLabel.setText(name);
+		updateCashLabel();
+		panel.add(nameLabel);
+		panel.add(cashLabel);
+		panel.add(vehicle.getPanel());	
+	}
+	
+	private void updateCashLabel()
+	{
+		cashLabel.setText("Cash: " + cash);
+		panel.repaint();
+	}
+	
+	private class PlayerSetupActionListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			if ( vehicle.isReady() )
+			{
+				name = nameTextField.getText();
+				panel.removeAll();
+				updatePanel();
+				panel.revalidate();
+				isValid = true;
+			}
+			else
+			{
+				enterNameLabel.setText("Please enter a name and select a vehicle");
+			}
+			
+		}
+	}
 }
