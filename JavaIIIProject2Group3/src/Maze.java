900406012455a2d2f51df46a0494f41972f3154a
CColon1 <sikz26300@gmail.com>
2015-05-15 17:11:55 -0400

    Maze class default constructor fills entire maze with random room objects, added exists(Direction) methods and move(Direction) methods

---
+++ JavaIIIProject2Group3/src/Maze.java
@@ -0,0 +1,75 @@
+
+public class Maze implements iMaze {
+	iRoom[][] maze = new iRoom[10][10];
+	
+	public Maze(){
+		for (int xPosition = 0;xPosition<10;xPosition++){
+			for(int yPosition = 0;yPosition<10;yPosition++){
+				maze[xPosition][yPosition] = new Room();	//randomly generates a room for ever position in maze[][]			
+			}
+		}
+	}
+
+	@Override
+	public boolean existsUp(int[] playerLocation) {
+		
+		if(!(this.maze[playerLocation[0]][playerLocation[1]-1]==null)){
+			return true;
+		}
+		else return false;
+	}
+
+	@Override
+	public boolean existsDown(int[] playerLocation) {
+		if(!(this.maze[playerLocation[0]][playerLocation[1]+1]==null)){
+			return true;
+		}
+		else return false;
+	}
+
+	@Override
+	public boolean existsLeft(int[] playerLocation) {
+		if(!(this.maze[playerLocation[0]-1][playerLocation[1]]==null)){
+			return true;
+		}
+		else return false;
+	}
+
+	@Override
+	public boolean existsRight(int[] playerLocation) {
+		if(!(this.maze[playerLocation[0]+1][playerLocation[1]]==null)){
+			return true;
+		}
+		else return false;
+	}
+
+	@Override
+	public int[] moveUp(int[] playerLocation) {
+		int[] newLocation = {playerLocation[0],playerLocation[1]-1};
+		return newLocation;
+	}
+
+	@Override
+	public int[] moveDown(int[] playerLocation) {
+		int[] newLocation = {playerLocation[0],playerLocation[1]+1};
+		return newLocation;
+	}
+
+	@Override
+	public int[] moveLeft(int[] playerLocation) {
+		int[] newLocation = {playerLocation[0]-1,playerLocation[1]};
+		return newLocation;
+	}
+
+	@Override
+	public int[] moveRight(int[] playerLocation) {
+		int[] newLocation = {playerLocation[0]+1,playerLocation[1]};
+		return newLocation;
+	}
+
+	@Override
+	public void buildMaze() {
+		// to build maze from local config file. feature in backlog.
+		
+	}
+}
