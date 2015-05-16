1b86eb95ef66288d8119c8ba4c934984e0ded7af
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-15 21:57:05 -0400

    Added fields and methods to Character

---
+++ JavaIIIProject2Group3/src/Character.java
@@ -0,0 +1,173 @@
+
+public class Character implements iCharacter {
+
+	// fields
+	private double strength; // warrior
+	private double dexterity; // thief
+	private double hitPoints; // all
+	private double intelligence; // wizard
+	private int charLocation; // character location
+	private String[] items;
+	private String charType; // TEMP until specified classes are made
+	private String charName; // player's name
+	
+	public Character() {}
+	
+	// TEMP: all attributes are standard
+	public Character(String charName, String charType) {
+		this.strength = 5.0;
+		this.dexterity = 2.0;
+		this.hitPoints = 20.0;
+		this.intelligence = 6.0;
+		this.charLocation = 1;
+	}
+	
+	public void addArmor(double armor) {
+		this.hitPoints += armor;
+	}
+	
+	/**
+	 * @return the strength
+	 */
+	public double getStrength() {
+		return strength;
+	}
+
+
+
+	/**
+	 * @param strength the strength to set
+	 */
+	public void setStrength(double strength) {
+		this.strength = strength;
+	}
+
+
+
+	/**
+	 * @return the dexterity
+	 */
+	public double getDexterity() {
+		return dexterity;
+	}
+
+
+
+	/**
+	 * @param dexterity the dexterity to set
+	 */
+	public void setDexterity(double dexterity) {
+		this.dexterity = dexterity;
+	}
+
+
+
+	/**
+	 * @return the hitPoints
+	 */
+	public double getHitPoints() {
+		return hitPoints;
+	}
+
+
+
+	/**
+	 * @param hitPoints the hitPoints to set
+	 */
+	public void setHitPoints(double hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+
+
+	/**
+	 * @return the intelligence
+	 */
+	public double getIntelligence() {
+		return intelligence;
+	}
+
+
+
+	/**
+	 * @param intelligence the intelligence to set
+	 */
+	public void setIntelligence(double intelligence) {
+		this.intelligence = intelligence;
+	}
+
+
+
+	/**
+	 * @return the charLocation
+	 */
+	public int getCharLocation() {
+		return charLocation;
+	}
+
+
+
+	/**
+	 * @param charLocation the charLocation to set
+	 */
+	public void setCharLocation(int charLocation) {
+		this.charLocation = charLocation;
+	}
+
+
+
+	/**
+	 * @return the charType
+	 */
+	public String getCharType() {
+		return charType;
+	}
+
+
+
+	/**
+	 * @param charType the charType to set
+	 */
+	public void setCharType(String charType) {
+		this.charType = charType;
+	}
+
+
+
+	/**
+	 * @return the charName
+	 */
+	public String getCharName() {
+		return charName;
+	}
+
+
+
+	/**
+	 * @param charName the charName to set
+	 */
+	public void setCharName(String charName) {
+		this.charName = charName;
+	}
+
+
+
+	// methods
+	@Override
+	public int generateMvmt() {
+		return 0;
+	}
+
+	@Override
+	public void pickUpItem(String item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drinkPotion(double potion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
