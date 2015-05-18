be799921ecd8c9c5556f51bff3f4f0b49018fcdd
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-18 19:56:39 -0400

    Add shells for ICharacterFactory, PlayerCharacterFactory and WizardCharacterFactory example

---
+++ JavaIIIProject2GroupProfs/src/PlayerCharacterFactory.java
@@ -0,0 +1,10 @@
+
+public class PlayerCharacterFactory implements ICharacterFactory {
+
+	@Override
+	public ICharacter createCharacter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
