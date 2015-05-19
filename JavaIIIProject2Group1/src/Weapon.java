81573f48e6299789d75bdeb85ab960fbccd81e89
Darrell Purcell <dpsvr2@gmail.com>
2015-05-19 14:02:53 -0400

    Weapon Class

---
+++ JavaIIIProject2Group1/src/Weapon.java
@@ -0,0 +1,48 @@
+import javax.swing.JPanel;
+
+public class Weapon implements Item 
+
+{
+	
+	private String name;
+	private int durability;
+	private int baseDamage;
+	
+	public Weapon (String name, int durability, int baseDamage)
+	{
+		
+		this.name = name;
+		this.durability = durability;
+		this.baseDamage = baseDamage;
+	}
+		
+	@Override
+	public String getName()
+	{
+		
+		return null;
+	}
+
+	@Override
+	public int getDurability() 
+	{
+		
+		return 0;
+	}
+
+	@Override
+	public boolean isBroken() 
+	{
+		
+		return false;
+	}
+
+	@Override
+	public JPanel getPanel() 
+	{
+		
+		return null;
+	}
+	
+}
+
