622a185c340980d3c9a55fbccfdcaf1c9f5e073c
Sarah Johnston <johnston-s@live.com>
2015-05-19 10:04:49 -0400

    Room again

--- JavaIIIProject2Group4/src/Room.java
+++ JavaIIIProject2Group4/src/Room.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class Room {
 
+	//contstructor
+	public Room(String string) {
+	
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	}
+	Scanner in = new Scanner(System.in);
+
+	public static final Room NORTH = new Room("North");
+	public static final Room EAST  = new Room("East");
+	public static final Room SOUTH = new Room("South");
+	public static final Room WEST  = new Room("West");
+
+	
 
 	}
 
-}
+
