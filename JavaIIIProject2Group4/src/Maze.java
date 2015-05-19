2018af8d82f908c131e98ed568fef620dd09f031
Sarah Johnston <johnston-s@live.com>
2015-05-19 10:03:18 -0400

    Room Class

---
+++ JavaIIIProject2Group4/src/Maze.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Maze {
+	
+	public static void main (String[] args) {
+		
+		
+		  ArrayList<String> beasts = new ArrayList<String>();
+		  beasts.add("Orc");
+		  beasts.add("Dragon");
+		  beasts.add("Wolf");
+		  
+		  
+	
+	
+	    
+	    
+	    
+	}//end public class
+		
+}//end main
