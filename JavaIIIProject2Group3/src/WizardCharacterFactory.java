47b5a722b10200b69670743040a10519604e05ae
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-18 20:32:01 -0400

    Deleted NPC and Player classes

---
+++ JavaIIIProject2Group3/src/WizardCharacterFactory.java
@@ -0,0 +1,14 @@
+
+public class WizardCharacterFactory implements iCharacterFactory {
+
+	public WizardCharacterFactory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public iCharacter createCharacter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
