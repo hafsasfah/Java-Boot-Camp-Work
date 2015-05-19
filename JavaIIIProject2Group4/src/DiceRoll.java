e98b70651d683b6e9f4e573bde99021399133daf
Sarah Johnston <johnston-s@live.com>
2015-05-19 10:54:23 -0400

    dice roll correction

--- JavaIIIProject2Group4/src/DiceRoll.java
+++ JavaIIIProject2Group4/src/DiceRoll.java
@@ -2,15 +2,11 @@ import java.util.Random;
 
 
 public class DiceRoll {
-	public void Dice (){
-		Random r = new Random();
-		Random r2 = new Random();
-		Random r3 = new Random();
-
-		
-		int dice3t1 = r.nextInt(6)+1;
-		int dice3t2 = r2.nextInt(6)+1; 
-		int dice3t3 = r3.nextInt(6)+1; 
-
-		}
+	Random r = new Random();
+	Random r2 = new Random();
+	Random r3 = new Random();
+	
+	int dice3t1 = r.nextInt(6)+1;
+	int dice3t2 = r2.nextInt(6)+1; 
+	int dice3t3 = r3.nextInt(6)+1; 
 }
