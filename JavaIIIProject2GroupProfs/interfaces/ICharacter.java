3debb288263803684a4657434fa649a6b264a85a
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-20 01:20:49 -0400

    Add weapon, armor and gold to ICharacter

--- JavaIIIProject2GroupProfs/interfaces/ICharacter.java
+++ JavaIIIProject2GroupProfs/interfaces/ICharacter.java
@@ -3,13 +3,17 @@ import javax.swing.JPanel;
 
 public interface ICharacter {
 	String getName();
+	int getHitPoints();
 	int getStrength();
 	int getIntelligence();
 	int getDexterity();
-	int getHitPoints();
+	int getGold();
+	void setGold( int gold );
 	boolean isAlive();
 	String attack( ICharacter target );
 	IItem getWeapon();
+	void setWeapon( IItem weapon );
 	IItem getArmor();
+	void setArmor( IItem armor );
 	JPanel getPanel();
 }
