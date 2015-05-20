eb1f3d2051c95aa78f773c9243c44326aa1bdc94
dglenn248 <dglenn55@hotmail.com>
2015-05-20 09:58:01 -0400

    Adding imaze and iroom test

---
+++ JavaIIIProject2Group1/src/iMazeTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class iMazeTest {
+
+	@Test
+	public void testGetStartingPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEndPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsValidPosition() {
+		fail("Not yet implemented");
+	}
+
+}
