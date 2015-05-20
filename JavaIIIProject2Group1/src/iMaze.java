56a53f4c60805583a79565941c7afb566f438632
dglenn248 <dglenn55@hotmail.com>
2015-05-20 09:42:23 -0400

    Adding IMaze and IRoom interfaces

---
+++ JavaIIIProject2Group1/src/iMaze.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+import javax.swing.JPanel;
+
+public interface iMaze 
+{
+	
+		iPosition getStartingPosition();
+		Iterator<iPosition> iterator( iPosition position );
+		boolean isEndPosition( iPosition position );
+		boolean isValidPosition( iPosition position );
+		JPanel getPanel();
+	
+		
+		
+		
+		
+		}
+	
+
+	
+	 
+	
+		
+		
+
---
+++ JavaIIIProject2Group1/src/iRoom.java
@@ -0,0 +1,16 @@
+import javax.swing.JPanel;
+
+
+public interface iRoom 
+{
+	String getName();
+	iParty getNPCParty();
+	JPanel getPanel();
+	int getGold();
+}
+
+
+	
+		
+		
+	
