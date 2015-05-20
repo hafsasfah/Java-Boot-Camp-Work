c416e6c49ce4d4bcaed2571d18d94da6cca5292f
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-20 09:05:28 -0400

    Working on attack method

--- JavaIIIProject2Group3/src/Action.java
+++ JavaIIIProject2Group3/src/Action.java
@@ -1,7 +1,29 @@
 public class Action {
 
-	public static int attack(int attackerStrength, int opponentHitPoints) {
-		return attackerStrength - opponentHitPoints;
+	public static void attack(Party players, Party NPC) {	
+		Character[] pc = players.getCharacter();
+		Character[] npc = NPC.getCharacter();
+		
+		int pcTotalAttack = 0; 
+		int npcTotalAttack = 0;
+		int pcSize = pc.length;
+		int npcSize = npc.length;
+		
+		for (Character c : pc) {
+			pcTotalAttack += c.getStrength();
+		}
+		
+		pcTotalAttack /= npcSize;
+		
+		for (Character nc : npc) {
+			npcTotalAttack+= nc.getStrength();
+		}
+		
+		npcTotalAttack /= pcSize;
+		
+		players.setCharacter(pc);
+		NPC.setCharacter(npc);
+		
 	}
 	
 	public static int sleep(int playerHitPoints) {
