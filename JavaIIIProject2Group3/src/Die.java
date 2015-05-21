4157fc714a8c15f0b679a1cf180bab1fc46ad426
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-21 13:39:49 -0400

    Fixed Die class
    as per Eric's comment

--- JavaIIIProject2Group3/src/Die.java
+++ JavaIIIProject2Group3/src/Die.java
@@ -1,34 +1,41 @@
+import java.util.Random;
+
 
 public class Die {
+	private static Random random;
 	
 	public static int roll3D6(){ // For Player Stats: Str, Dex, Int
+		random = new Random();
 		int sides;
 		int total = 0;
 		int rolls = 3;
 		for (int i = 0; i < rolls; i++){
-			sides = (int) Math.floor(Math.random()*6) + 1; //3D6
+			sides = random.nextInt(6) + 1; //3D6
 			total += sides;
 		}
 		return total;
 	}
 	
 	public static int roll1To6D3() { // For NPC Stats: Str, Dex, Int, HP
+		random = new Random();
 		int sides; 
 		int total = 0;
-		int rolls = (int) Math.floor(Math.random()*6) + 1;
+		int rolls = random.nextInt(6) + 1;
 		for (int i = 0; i < rolls; i++){
-			sides = (int) Math.floor(Math.random()*3) + 1;
+			sides = random.nextInt(3) + 1;
 			total += sides;
 		}
 		return total;
 	}
 	
 	public static int roll1D15() { // Some attacks.
-		return (int) Math.floor(Math.random()*15) + 1;
+		random = new Random();
+		return (int) random.nextInt(15) + 1;
 	}
 	
 	public static int roll1D20() { // Find gold. Some attacks.
-		return (int) Math.floor(Math.random()*20) + 1;
+		random = new Random();
+		return (int) random.nextInt(20) + 1;
 	}
 
 }
