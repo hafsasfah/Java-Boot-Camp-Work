9ea55d0669dcac61c4bf38605c4ab98be6cb9857
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-21 14:28:28 -0400

    reformatted Party class
    as per Eric

--- JavaIIIProject2Group3/src/Party.java
+++ JavaIIIProject2Group3/src/Party.java
@@ -9,36 +9,32 @@ import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-
-
-
-
 public class Party {
-	//private PlayerCharacterFactory playerFactory ;
+	// private PlayerCharacterFactory playerFactory ;
 	private Location location;
-	private iCharacter[] characters = new Character[6] ;
+	private iCharacter[] characters = new Character[6];
 
 	// GUI fields
 	private JPanel partyPanel;
-		//subPanels
-		private JPanel movementButtonsPanel; 
-		private JPanel actionsButtonsPanel; 
-			//sub action button panels
-			private JPanel actionstopButtonsPanel;
-			private JPanel actionsbottomButtonsPanel;
-				//actionButtons
-				private JButton searchButton;
-				private JButton sleepButton;
-				private JButton attackButton;
-				private JButton runButton;
-				private JButton suicideButton;
-			//movementButtons
-			private JButton northButton;
-			private JButton downButton;	
-			private JButton leftButton;	
-			private JButton rightButton; 
-	
-	public Party(iCharacter[] characters){
+	// subPanels
+	private JPanel movementButtonsPanel;
+	private JPanel actionsButtonsPanel;
+	// sub action button panels
+	private JPanel actionstopButtonsPanel;
+	private JPanel actionsbottomButtonsPanel;
+	// actionButtons
+	private JButton searchButton;
+	private JButton sleepButton;
+	private JButton attackButton;
+	private JButton runButton;
+	private JButton suicideButton;
+	// movementButtons
+	private JButton northButton;
+	private JButton downButton;
+	private JButton leftButton;
+	private JButton rightButton;
+
+	public Party(iCharacter[] characters) {
 		location = new Location();
 
 		this.characters = characters;
@@ -46,62 +42,62 @@ public class Party {
 
 	}
 
-	public iCharacter[] getCharacter(){
+	public iCharacter[] getCharacter() {
 		return characters;
 	}
-	public void setCharacter(iCharacter[] update){
+
+	public void setCharacter(iCharacter[] update) {
 		characters = update;
 	}
-	
-	
-	
+
 	private void generateGUI() {
-		//Creating party panel
+		// Creating party panel
 		partyPanel = new JPanel();
 		partyPanel.setLayout(new BorderLayout());
-		for(iCharacter c: characters){
+		for (iCharacter c : characters) {
 			partyPanel.add(c.getPanel());
-			
+
 		}
-		
-		
-		// Create MovementButtons  Panel
-		this.movementButtonsPanel = new JPanel();							
+
+		// Create MovementButtons Panel
+		this.movementButtonsPanel = new JPanel();
 		this.movementButtonsPanel.setLayout(new BorderLayout());
-		
-		//Create Action panels and sub panels
+
+		// Create Action panels and sub panels
 		this.actionsButtonsPanel = new JPanel();
 		this.actionsButtonsPanel.setLayout(new BorderLayout());
 		actionstopButtonsPanel = new JPanel();
 		actionstopButtonsPanel.setLayout(new BorderLayout());
-		this.actionsButtonsPanel.add(actionstopButtonsPanel,BorderLayout.NORTH);
+		this.actionsButtonsPanel
+				.add(actionstopButtonsPanel, BorderLayout.NORTH);
 		actionsbottomButtonsPanel = new JPanel();
 		actionsbottomButtonsPanel.setLayout(new BorderLayout());
-		this.actionsButtonsPanel.add(actionsbottomButtonsPanel,BorderLayout.SOUTH);
-				
-		//Action buttons added to button panel
+		this.actionsButtonsPanel.add(actionsbottomButtonsPanel,
+				BorderLayout.SOUTH);
+
+		// Action buttons added to button panel
 		searchButton = new JButton("Search ");
-		this.actionstopButtonsPanel.add(searchButton,BorderLayout.WEST);
+		this.actionstopButtonsPanel.add(searchButton, BorderLayout.WEST);
 		sleepButton = new JButton(" Sleep");
-		this.actionstopButtonsPanel.add(sleepButton,BorderLayout.EAST);
+		this.actionstopButtonsPanel.add(sleepButton, BorderLayout.EAST);
 		attackButton = new JButton("Attack");
-		this.actionsbottomButtonsPanel.add(attackButton,BorderLayout.EAST);
+		this.actionsbottomButtonsPanel.add(attackButton, BorderLayout.EAST);
 		runButton = new JButton("    Run   ");
-		this.actionsbottomButtonsPanel.add(runButton,BorderLayout.WEST);
+		this.actionsbottomButtonsPanel.add(runButton, BorderLayout.WEST);
 		suicideButton = new JButton(" Hara Kiri ");
-		this.actionstopButtonsPanel.add(suicideButton,BorderLayout.SOUTH);
-		
-		//Adds movement buttons to panel
+		this.actionstopButtonsPanel.add(suicideButton, BorderLayout.SOUTH);
+
+		// Adds movement buttons to panel
 		northButton = new JButton("North");
-		this.movementButtonsPanel.add(northButton,BorderLayout.PAGE_START);
+		this.movementButtonsPanel.add(northButton, BorderLayout.PAGE_START);
 		downButton = new JButton("South");
-		this.movementButtonsPanel.add(downButton,BorderLayout.PAGE_END);
+		this.movementButtonsPanel.add(downButton, BorderLayout.PAGE_END);
 		leftButton = new JButton("West");
-		this.movementButtonsPanel.add(leftButton,BorderLayout.LINE_START);
+		this.movementButtonsPanel.add(leftButton, BorderLayout.LINE_START);
 		rightButton = new JButton("East");
-		this.movementButtonsPanel.add(rightButton,BorderLayout.LINE_END);
-					
-		//Event handling for buttons
+		this.movementButtonsPanel.add(rightButton, BorderLayout.LINE_END);
+
+		// Event handling for buttons
 		Listener handler = new Listener();
 		northButton.addActionListener(handler);
 		downButton.addActionListener(handler);
@@ -112,139 +108,137 @@ public class Party {
 		attackButton.addActionListener(handler);
 		runButton.addActionListener(handler);
 		suicideButton.addActionListener(handler);
-		try {screenUpdate();}
-		catch(Exception e){
+		try {
+			screenUpdate();
+		} catch (Exception e) {
 			;
 		}
-				
-				
-				
-				
-				
-		
+
 	}
-
-
 
 	public String getCharLocationString() {
 		return location.toString();
 	}
 
-	public void screenUpdate() throws Exception{
+	public void screenUpdate() throws Exception {
 		buttonCheck();
-		//clearConsole();
+		// clearConsole();
 	}
-	
-	public JPanel getPartyPanel(){
+
+	public JPanel getPartyPanel() {
 		return partyPanel;
 	}
-	
-	public JPanel getActionPanel(){
+
+	public JPanel getActionPanel() {
 		return this.actionsButtonsPanel;
 	}
-	
-	public JPanel getMovementPanel(){
+
+	public JPanel getMovementPanel() {
 		return movementButtonsPanel;
 	}
-	
-	private void buttonCheck() throws Exception{
+
+	private void buttonCheck() throws Exception {
 		int[] playerLocation = location.getLocation();
 		if (!(GUI.getMaze().canGoNorth(playerLocation))) {
 			northButton.setEnabled(false);
-		}
-		else northButton.setEnabled(true);
+		} else
+			northButton.setEnabled(true);
 		if (!(GUI.getMaze().canGoSouth(playerLocation))) {
 			downButton.setEnabled(false);
-		} else downButton.setEnabled(true);
+		} else
+			downButton.setEnabled(true);
 		if (!(GUI.getMaze().canGoWest(playerLocation))) {
 			leftButton.setEnabled(false);
-		} else leftButton.setEnabled(true);
+		} else
+			leftButton.setEnabled(true);
 		if (!(GUI.getMaze().canGoEast(playerLocation))) {
 			rightButton.setEnabled(false);
-		} else rightButton.setEnabled(true);
-		
+		} else
+			rightButton.setEnabled(true);
 
-		//if statements needed for action buttons
+		// if statements needed for action buttons
 		searchButton.setEnabled(true);
-		
+
 		sleepButton.setEnabled(true);
-		
+
 		attackButton.setEnabled(true);
-		
+
 		runButton.setEnabled(true);
-		
+
 		suicideButton.setEnabled(true);
-		
-		
-		
+
 	}
-	public void updateAllPanels(){
-		for(iCharacter c : characters){
+
+	public void updateAllPanels() {
+		for (iCharacter c : characters) {
 			c.panelUpdate();
 		}
 	}
-	public Party getParty(){
+
+	public Party getParty() {
 		return this;
 	}
-	
-	private class Listener  implements ActionListener  {
-		
+
+	private class Listener implements ActionListener {
+
 		public void actionPerformed(ActionEvent event) {
-		
+
 			if (event.getSource() == northButton) {
 				location.moveUp();
-				try {screenUpdate();}
-				catch(Exception e){
+				try {
+					screenUpdate();
+				} catch (Exception e) {
 					;
 				}
 				GUI.setConsole("North \n");
-				
-			}	
-			else if (event.getSource() == downButton) {
+
+			} else if (event.getSource() == downButton) {
 				location.moveDown();
-				try {screenUpdate();}
-				catch(Exception e){
+				try {
+					screenUpdate();
+				} catch (Exception e) {
 					;
 				}
 				GUI.setConsole("South \n");
-			}
-			else if (event.getSource() == leftButton) {
+			} else if (event.getSource() == leftButton) {
 				location.moveLeft();
-				try {screenUpdate();}
-				catch(Exception e){
+				try {
+					screenUpdate();
+				} catch (Exception e) {
 					;
 				}
 				GUI.setConsole("West \n");
-			}
-			else if (event.getSource() == rightButton) {
+			} else if (event.getSource() == rightButton) {
 				location.moveRight();
-				try {screenUpdate();}
-				catch(Exception e){
+				try {
+					screenUpdate();
+				} catch (Exception e) {
 					;
 				}
 				GUI.setConsole("East\n");
 			}
-			
+
 			else if (event.getSource() == searchButton) {
-				try {buttonCheck();}
-				catch(Exception e){
+				try {
+					buttonCheck();
+				} catch (Exception e) {
 					;
 				}
 				GUI.appendConsole("The Room is empty.....oh so very Empty\n");
-				// add function here					
-				}	
-			else if (event.getSource() == sleepButton) {
-				try {buttonCheck();}
-				catch(Exception e){
+				// add function here
+			} else if (event.getSource() == sleepButton) {
+				try {
+					buttonCheck();
+				} catch (Exception e) {
 					;
 				}
-				//Action.sleep(characters.getHitPoints());
+				// Action.sleep(characters.getHitPoints());
 				GUI.appendConsole("You had a siesta, feel better?\n");
-			}
-			else if (event.getSource() == attackButton) {
-				try {buttonCheck();
-				updateAllPanels();}
-				catch(Exception e){
+			} else if (event.getSource() == attackButton) {
+				try {
+					buttonCheck();
+					updateAllPanels();
+				} catch (Exception e) {
 					;
 				}
 				try {
@@ -252,23 +246,25 @@ public class Party {
 				} catch (Exception e) {
 					;
 				}
-				
-				Action.attack(getParty(), GUI.getMaze().getRoom(location.getLocation()).getParty());
-			//	GUI.appendConsole("You attacked.........................a wall. "
-					//	+ "\nIf you had a weapon you probably just dented it\n"
-				//		+ "Good job ace. Real smooth\n");
-			}
-			else if (event.getSource() == runButton) {
-				try {buttonCheck();}
-				catch(Exception e){
+
+				Action.attack(getParty(),
+						GUI.getMaze().getRoom(location.getLocation())
+								.getParty());
+				// GUI.appendConsole("You attacked.........................a wall. "
+				// + "\nIf you had a weapon you probably just dented it\n"
+				// + "Good job ace. Real smooth\n");
+			} else if (event.getSource() == runButton) {
+				try {
+					buttonCheck();
+				} catch (Exception e) {
 					;
 				}
-				//Action.run(characters.getHitPoints());
+				// Action.run(characters.getHitPoints());
 				GUI.appendConsole("Running from shadows? Some adventurer you are.\n");
-			}
-			else if (event.getSource() == suicideButton) {
-				try {buttonCheck();}
-				catch(Exception e){
+			} else if (event.getSource() == suicideButton) {
+				try {
+					buttonCheck();
+				} catch (Exception e) {
 					;
 				}
 				Action.haraKiri();
@@ -276,25 +272,18 @@ public class Party {
 		}
 	}
 
-
-
 	public Location getLocation() {
 		return location;
 	}
-	
+
 	static void battleMusic() throws Exception {
 		URL url = new URL(
-	            "http://themushroomkingdom.net/sounds/wav/mk64/mk64_announcer04-jp.wav");
-	        Clip clip = AudioSystem.getClip();
-	        // getAudioInputStream() also accepts a File or InputStream
-	        AudioInputStream ais = AudioSystem.
-	            getAudioInputStream( url );
-	        clip.open(ais);
-	        clip.loop(Clip.LOOP_CONTINUOUSLY);
+				"http://themushroomkingdom.net/sounds/wav/mk64/mk64_announcer04-jp.wav");
+		Clip clip = AudioSystem.getClip();
+		// getAudioInputStream() also accepts a File or InputStream
+		AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+		clip.open(ais);
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
 	}
-	
-	
-	
-	
 
 }
