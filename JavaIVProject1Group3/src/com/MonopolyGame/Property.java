252ef4af8ed1090ad15028962819ff4abf9ab07b
mfmcmillian <matthew.mcmillian@ymail.com>
2015-05-22 15:30:01 -0400

    PropertyClass Start
    Dont worry i know we have to discuss more about the design i just wanted
    to get the github thing doing for me.

---
+++ JavaIVProject1Group3/src/com/MonopolyGame/Property.java
@@ -0,0 +1,48 @@
+package com.MonopolyGame;
+
+public class Property {
+
+	private String nameOfProperty;
+	private String ownerOfProperty;
+	private int rent;
+	private int costOfProperty;
+	
+public Property()
+{
+	
+	
+}
+
+public String getNameOfProperty() {
+	return nameOfProperty;
+}
+
+public void setNameOfProperty(String nameOfProperty) {
+	this.nameOfProperty = nameOfProperty;
+}
+
+public String getOwnerOfProperty() {
+	return ownerOfProperty;
+}
+
+public void setOwnerOfProperty(String ownerOfProperty) {
+	this.ownerOfProperty = ownerOfProperty;
+}
+
+public int getRent() {
+	return rent;
+}
+
+public void setRent(int rent) {
+	this.rent = rent;
+}
+
+public int getCostOfProperty() {
+	return costOfProperty;
+}
+
+public void setCostOfProperty(int costOfProperty) {
+	this.costOfProperty = costOfProperty;
+}
+	
+}
