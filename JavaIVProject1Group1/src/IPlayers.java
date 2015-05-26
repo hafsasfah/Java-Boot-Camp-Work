e79d233a4809026f7241bbf3fc6d6123fe0f6bb0
dglenn248 <dglenn55@hotmail.com>
2015-05-26 11:16:52 -0400

    Adding iPlayer and Player

---
+++ JavaIVProject1Group1/src/IPlayers.java
@@ -0,0 +1,13 @@
+
+public interface IPlayers 
+{
+	String getMove();
+	int getCash();
+	int getPayRent();
+	int setLocation();
+	int setCoordinates();
+	int getPurchase();
+	
+	
+	
+}
---
+++ JavaIVProject1Group1/src/Players.java
@@ -0,0 +1,57 @@
+
+public class Players implements IPlayers 
+{
+	String move;
+	int purchase;
+	int payRent;
+	int location;
+	int coordinates;
+	int cash;
+	
+	public Players(String move, int purchase, int payRent, int location, int coordinates)
+	{
+		this.move = move;
+		this.purchase = purchase;
+		this.payRent = payRent;
+		this.cash = cash;
+		
+	}
+
+	@Override
+	public String getMove() {
+		
+		return this.move;
+	}
+
+	
+	@Override
+	public int getPayRent() {
+		
+		return this.payRent;
+	}
+
+	@Override
+	public int getCash() {
+		
+		return this.cash;
+	}
+
+	@Override
+	public int setLocation() {
+		
+		return location;
+	}
+
+	@Override
+	public int setCoordinates() {
+		
+		return coordinates;
+	}
+
+	@Override
+	public int getPurchase() {
+		
+		return this.purchase;
+	}
+
+}
