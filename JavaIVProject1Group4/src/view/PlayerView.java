64df27ee1717bcfa81a146610236f99f26b7f6fe
CColon1 <sikz26300@gmail.com>
2015-05-26 15:59:45 -0400

    started gui classes

--- JavaIVProject1Group4/src/view/PlayerView.java
+++ JavaIVProject1Group4/src/view/PlayerView.java
@@ -1,5 +1,23 @@
 package view;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Player;
+
 public class PlayerView {
+	JPanel panel ;
+	JLabel label;
+	
+	public PlayerView(Player player){
+		panel = new JPanel();
+		label = new JLabel();
+		label.setText("<html>"+player.getName()+"<br>" +player.getCash() + "</html>");
+		panel.add(label);
+				
+	}
+	public JPanel getPanel(){
+		return panel;
+	}
 
 }
