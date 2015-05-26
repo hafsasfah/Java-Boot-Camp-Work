077c8d5e4cde27e02ed750836398e636b64efffc
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-26 10:52:41 -0400

    Created interfaces folder

---
+++ JavaIVProject1Group1/interfaces/iProperty.java
@@ -0,0 +1,10 @@
+
+public interface iProperty 
+{
+
+	void assignPlayerToProperty(iPlayer player);
+	void chargePlayerRent(int rent);
+	Coordinates coord = new Coordinates();
+	String getName();
+	
+}
