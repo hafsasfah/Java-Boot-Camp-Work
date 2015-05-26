384649637ada23948c7b35eb81129cead5dfcc25
Sarah Johnston <johnston-s@live.com>
2015-05-26 10:50:08 -0400

    diceroll

--- Group1/Game/DiceRoll.java
+++ Group1/Game/DiceRoll.java
@@ -1,17 +1,10 @@
 import java.util.Random;
 public class DiceRoll {
 	
-
-
 			static Random r =  new Random();
 			static Random r2 = new Random();
-			static Random r3 = new Random();
 		
-			
-			static int roll1 = r.nextInt(6)+1;
-			static int roll2  = r2.nextInt(6)+1; 
-			static int roll3  = r3.nextInt(6)+1;
-			
-				
+			static int roll1 = r.nextInt(6);
+			static int roll2  = r2.nextInt(6); 			
 			
 }
