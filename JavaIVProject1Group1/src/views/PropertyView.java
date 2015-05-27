73cb1c6a5ab8c840845bb67e72d69a480d931bb3
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-26 21:19:08 -0400

    PropertyView

---
+++ JavaIVProject1Group1/src/views/PropertyView.java
@@ -0,0 +1,20 @@
+package views;
+
+import java.util.Observable;
+import javax.swing.JPanel;
+
+public class PropertyView implements iPropertyView {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JPanel getPlayerPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
