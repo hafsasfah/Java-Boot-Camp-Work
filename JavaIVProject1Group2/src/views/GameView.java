981d6d8f7fbc2871de67570480ec258ac7c0b1e8
Tommiec21 <Tommiec22@gmail.com>
2015-05-27 14:17:21 -0400

    Some stuff 2
    yup

---
+++ JavaIVProject1Group2/src/views/GameView.java
@@ -0,0 +1,23 @@
+package views;
+import java.util.*;
+
+import javax.swing.JPanel;
+public class GameView<Monopoly>  implements IView{
+	
+	public GameView(Monopoly game) {
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JPanel getGameView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
--- JavaIVProject1Group2/src/views/ViewProperty.java
+++ JavaIVProject1Group2/src/views/ViewProperty.java
@@ -1,4 +1,4 @@
-package models;
+package views;
 import java.util.Observable;
 import java.util.Observer;
 
