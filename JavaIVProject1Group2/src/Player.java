4f59e1a574401c2d9ffd379de7e4670df9d4657d
plake06 <Pbl8806@mail.com>
2015-05-27 09:43:55 -0400

    update Player class

--- JavaIVProject1Group2/src/Player.java
+++ JavaIVProject1Group2/src/Player.java
@@ -1,5 +1,59 @@
+import java.util.HashSet;
+
+
+
 
 
 public class Player {
+	private int id;
+	private String name;
+	private int money;
+
+	
+	public Player( int id, String name, int money )
+	{
+		this.id = id;
+		this.name = name;
+		this.money = money;
+		
+	}
+	
+	public int getID() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public int getMoney() {
+		return money;
+	}
+
+
+	public void spendMoney(int money) {
+		this.money -= money;
+		setChanged();
+		notifyObservers();
+	}
+
+
+	private void notifyObservers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setChanged() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean hasLostGame() {
+		return money <= 0;
+	}
+
+
 
 }
