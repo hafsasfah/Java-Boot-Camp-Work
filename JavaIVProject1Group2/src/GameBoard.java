b7bdb497583ce52e3696bf0a42873a28c1458671
Sarah Johnston <johnston-s@live.com>
2015-05-27 15:42:29 -0400

    gameboard

--- JavaIVProject1Group2/src/GameBoard.java
+++ JavaIVProject1Group2/src/GameBoard.java
@@ -1,6 +1,9 @@
 
 
+
+
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +21,10 @@ import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
+import javax.swing.ImageIcon;
 
 //******Game board will build on the overall design of the game******//
+
 public class GameBoard extends JFrame 
 {
 	
@@ -61,10 +66,15 @@ public class GameBoard extends JFrame
 		setTitle("Monopoly");
 		setSize(1000,700);
 		setPreferredSize(new Dimension(1000, 700));
+		
+		JLabel bank = new JLabel("Bank");
+		getContentPane().add(bank, BorderLayout.NORTH);
 		createThePanels();
 		
 		
 		
+		
+	
 		
 	}
 	
@@ -76,12 +86,55 @@ public class GameBoard extends JFrame
 		GridBagLayout accessLayout = new GridBagLayout();
 		
 		accessLayout.rowWeights = new double[]	{ 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
-												0.1, 0.1, 0.1, 0.1, 0.1, 0.2}; 
+				0.1, 0.1, 0.1, 0.1, 0.1, 0.2}; 
 		
 		accessLayout.columnWeights = new double[] { 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
-													0.1, 0.1, 0.1, 0.1, 0.1, 0.2}; 
+				0.1, 0.1, 0.1, 0.1, 0.1, 0.2}; 
+		
 		
 		getContentPane().setLayout(accessLayout);
+		
+		JLabel jail = new JLabel("Jail");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 0;
+		getContentPane().add(jail, gbc_btnNewButton);
+		
+		getContentPane().setLayout(accessLayout);
+		
+		JLabel salon = new JLabel("Salon");
+		GridBagConstraints gbc_salon = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		getContentPane().add(salon, gbc_salon);
+		
+		getContentPane().setLayout(accessLayout);
+		
+		JLabel partyStore = new JLabel("Party Store");
+		GridBagConstraints gbc_partyStore = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		getContentPane().add(partyStore, gbc_partyStore);
+		
+		getContentPane().setLayout(accessLayout);
+		
+		JLabel playboyMansion = new JLabel("Playboy Mansion");
+		GridBagConstraints gbc_playboyMansion = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		getContentPane().add(playboyMansion, gbc_playboyMansion);
+		
+		getContentPane().setLayout(accessLayout);
+		
+		//JButton playboyMansion = new JButton("Playboy Mansion");
+		//GridBagConstraints gbc_playboyMansion = new GridBagConstraints();
+		//gbc_btnNewButton.gridx = 2;
+		//gbc_btnNewButton.gridy = 2;
+		//getContentPane().add(playboyMansion, gbc_playboyMansion);
+		
+		
+		
+		
 		
 
 		for(int j = 0; j < 4; j++)				//<-----------this represents the four sides Top, down, left, and right
--- JavaIVProject1Group2/src/data/GameRepository.java
+++ JavaIVProject1Group2/src/data/GameRepository.java
@@ -1,5 +1,36 @@
 package data;
 
-public class GameRepository {
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+
+public class GameRepository {
+   public static void main( String args[] )
+     {
+       Connection c = null;
+       Statement stmt = null;
+       try {
+         Class.forName("org.postgresql.Driver");
+         c = DriverManager
+            .getConnection("jdbc:postgresql://localhost:5432/Monopoly");
+         System.out.println("Opened database successfully");
+
+         stmt = c.createStatement();
+         String sql = "CREATE TABLE COMPANY " +
+                      "(ID INT PRIMARY KEY     NOT NULL," +
+                      " NAME           TEXT    NOT NULL, " +
+                      " AGE            INT     NOT NULL, " +
+                      " ADDRESS        CHAR(50), " +
+                      " SALARY         REAL)";
+         stmt.executeUpdate(sql);
+         stmt.close();
+         c.close();
+       } catch ( Exception e ) {
+         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+         System.exit(0);
+       }
+       System.out.println("Table created successfully");
+     }
 }
