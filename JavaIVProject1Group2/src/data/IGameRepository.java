1d9cc85862ac53d48b3b45981615d202b77a231f
Sarah Johnston <johnston-s@live.com>
2015-05-27 12:01:18 -0400

    repository

--- JavaIVProject1Group2/src/data/IGameRepository.java
+++ JavaIVProject1Group2/src/data/IGameRepository.java
@@ -1,5 +1,5 @@
 package data;
 
-public interface IGameRepository {
+public interface iGameRepository {
 
 }
--- JavaIVProject1Group2/src/data/iPlayerRepository.java
+++ JavaIVProject1Group2/src/data/iPlayerRepository.java
@@ -1,5 +1,5 @@
 package data;
 
-public class iPlayerRepository {
+public interface iPlayerRepository {
 
 }
