9a9bd48a14e1b0cf3f3d59fb4c5bfe4f3210e0f0
Sarah Johnston <johnston-s@live.com>
2015-05-27 10:00:34 -0400

    packages repair

---
+++ JavaIVProject1Group2/src/logic/DiceRoll.java
@@ -0,0 +1,11 @@
+package logic;
+import java.util.Random;
+public class DiceRoll {
+	
+			static Random r =  new Random();
+			static Random r2 = new Random();
+		
+			static int roll1 = r.nextInt(6);
+			static int roll2  = r2.nextInt(6); 			
+			
+}
