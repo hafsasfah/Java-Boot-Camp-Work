508afbddc2e01d47eb932073ae49c3593e2dbf34
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-27 14:02:21 -0400

    iGameView extends Observer

--- JavaIVProject1Group1/src/views/GameView.java
+++ JavaIVProject1Group1/src/views/GameView.java
@@ -1,11 +1,19 @@
 package views;
 
+import java.util.Observable;
+
 import models.aGame;
 
 public class GameView implements iGameView {
 
 	public GameView(aGame game, GameController gameController) {
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- JavaIVProject1Group1/src/views/iGameView.java
+++ JavaIVProject1Group1/src/views/iGameView.java
@@ -1,5 +1,7 @@
 package views;
 
-public interface iGameView {
+import java.util.Observer;
+
+public interface iGameView extends Observer {
 
 }
