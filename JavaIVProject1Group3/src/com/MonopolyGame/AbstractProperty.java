9809c062c64a15fec4dd7965376277aec7d7395a
mfmcmillian <matthew.mcmillian@ymail.com>
2015-05-27 10:13:45 -0400

    AbstractProperty

---
+++ JavaIVProject1Group3/src/com/MonopolyGame/AbstractProperty.java
@@ -0,0 +1,15 @@
+package com.MonopolyGame;
+
+import java.util.Observable;
+
+public abstract class AbstractProperty extends Observable
+{
+	public abstract String getNameOfProperty();
+	public abstract void setNameOfProperty();
+	public abstract String getOwnerOfProperty();
+	public abstract String setOwnerOfProperty();
+	public abstract void getRent();
+	public abstract void setRent();
+	public abstract int getCostOfProperty();
+	public abstract void setCostOfProperty();
+}
--- JavaIVProject1Group3/src/com/MonopolyGame/iProperty.java
+++ JavaIVProject1Group3/src/com/MonopolyGame/iProperty.java
@@ -5,7 +5,9 @@ public interface iProperty {
 	public String getNameOfProperty();
 	public void setNameOfProperty();
 	public String getOwnerOfProperty();
+	public String setOwnerOfProperty();
 	public void getRent();
+	public void setRent();
 	public int getCostOfProperty();
 	public void setCostOfProperty();
 
