ab4a55373c0478d896a5535fc9d38e237d7140c1
hafsasfah <hafsa.vahidy@gmail.com>
2015-05-27 13:51:06 -0400

    smoothing things out

--- JavaIVProject1Group1/src/models/aGame.java
+++ JavaIVProject1Group1/src/models/aGame.java
@@ -1,5 +1,17 @@
 package models;
 
+import views.iGameView;
+
 public abstract class aGame {
+
+	public void addObserver(iGameView gameView) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void nextPlayersTurn(int i) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
