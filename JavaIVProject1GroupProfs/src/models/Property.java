7c614d701ad59f8bbeb4bd4cbff1d3c31aaceae1
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-28 17:30:08 -0400

    Create property class

---
+++ JavaIVProject1GroupProfs/src/models/Property.java
@@ -0,0 +1,83 @@
+package models;
+
+import java.util.LinkedList;
+
+public class Property extends AbstractProperty {
+
+	private int id;
+	private int sequenceNumber;
+	private int gameID;
+	private String name;
+	private int purchasePrice;
+	private int rentalPrice;
+	private AbstractPlayer owner;
+	private LinkedList<AbstractPlayer> parkedPlayers;
+	
+
+	public Property( int id, int sequenceNumber, String name, int purchasePrice, int rentalPrice, AbstractPlayer owner, LinkedList<AbstractPlayer> parkedPlayers, int gameID )
+	{
+		this.id = id;
+		this.sequenceNumber = sequenceNumber;
+		this.gameID = gameID;
+		this.name = name;
+		this.purchasePrice = purchasePrice;
+		this.rentalPrice = rentalPrice;
+		this.owner = owner;
+		this.parkedPlayers = parkedPlayers;
+	}
+	
+	@Override
+	public int getID() {
+		return id;
+	}
+
+	@Override
+	public void setID(int id) {
+		this.id = id;	
+	}
+	
+	@Override
+	public int getSequenceNumber() {
+		return sequenceNumber;
+	}
+
+	@Override
+	public int getGameID() {
+		return gameID;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	@Override
+	public int getRentalPrice() {
+		return rentalPrice;
+	}
+
+	@Override
+	public AbstractPlayer getOwner() {
+		return owner;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	@Override
+	public LinkedList<AbstractPlayer> getParkedPlayers() {
+		return parkedPlayers;
+	}
+
+	@Override
+	public int compareTo( AbstractProperty otherProperty ) {
+		return sequenceNumber - otherProperty.getSequenceNumber();
+	}
+}
