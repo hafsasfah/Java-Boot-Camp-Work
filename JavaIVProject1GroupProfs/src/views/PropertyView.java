d3c2991d8e7df8359338a989e969f88748acb5a2
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-28 17:30:42 -0400

    update property view to use update to set text

--- JavaIVProject1GroupProfs/src/views/PropertyView.java
+++ JavaIVProject1GroupProfs/src/views/PropertyView.java
@@ -13,27 +13,36 @@ public class PropertyView implements IPropertyView {
 	private AbstractProperty property;
 	private JPanel panel;
 	private JLabel nameLabel;
+	private JLabel ownerLabel;
 	private JLabel purchasePriceLabel;
 	private JLabel rentalPriceLabel;
 	private JLabel parkedPlayersLabel;
-
+	
 	public PropertyView( AbstractProperty property )
 	{
 		this.property = property;
 		panel = new JPanel();
 		nameLabel = new JLabel( property.getName() );
-		purchasePriceLabel = new JLabel( Integer.toString(property.getPurchasePrice()) );
-		rentalPriceLabel = new JLabel( Integer.toString(property.getRentalPrice() ) );
-		parkedPlayersLabel = new JLabel(createParkedPlayersLabelString());
+		ownerLabel = new JLabel();
+		purchasePriceLabel = new JLabel();
+		rentalPriceLabel = new JLabel();
+		parkedPlayersLabel = new JLabel();
+		
+		panel.add( nameLabel );
+		panel.add( ownerLabel );
+		panel.add( purchasePriceLabel );
+		panel.add( rentalPriceLabel );
+		panel.add( parkedPlayersLabel );
+		
+		update(null,null);
 	}
 	
 	@Override
 	public void update(Observable o, Object arg) {
-		nameLabel.setText( property.getName() );
-		purchasePriceLabel.setText( Integer.toString(property.getPurchasePrice()) );
-		rentalPriceLabel.setText( Integer.toString(property.getRentalPrice() ) );
-		parkedPlayersLabel.setText( createParkedPlayersLabelString());
-		
+		ownerLabel.setText( "Owner:" + property.getOwner().getName() );
+		purchasePriceLabel.setText( "Purchase Price: " + property.getPurchasePrice() );
+		rentalPriceLabel.setText( "Rental Price: " + property.getRentalPrice() );
+		parkedPlayersLabel.setText( createParkedPlayersLabelString() );
 	}
 	
 	@Override
