0405c451abcb7f04f538437ecc98568ef09200c9
Eric Charnesky <eric.charnesky@careevolution.com>
2015-05-28 17:30:00 -0400

    update AbstractProperty - add Comparable to sort by sequence

--- JavaIVProject1GroupProfs/src/models/AbstractProperty.java
+++ JavaIVProject1GroupProfs/src/models/AbstractProperty.java
@@ -2,8 +2,11 @@ package models;
 
 import java.util.*;
 
-public abstract class AbstractProperty extends Observable {
+public abstract class AbstractProperty extends Observable implements Comparable<AbstractProperty> {
 	public abstract int getID();
+	public abstract void setID( int id );
+	public abstract int getSequenceNumber();
+	public abstract int getGameID();
 	public abstract String getName();
 	public abstract int getPurchasePrice();
 	public abstract int getRentalPrice();
