4da5ef356a1773861710c37232da4209211ec5db
sgargesh <sachin.gargesh@gmail.com>
2015-05-28 15:02:51 -0400

    Updating board

--- JavaIVProject1Group3/src/models/Board.java
+++ JavaIVProject1Group3/src/models/Board.java
@@ -8,37 +8,43 @@ public class Board {
 	
 	static JPanel boardPanel;
 	
-	//public static void main(String[] args)
-	 static byte gridLayout[][]  =  {{1,1,1,1,1,1,1,1,1,1}, 
-									 {1,0,0,0,0,0,0,0,0,1}, 
-								     {1,0,0,0,0,0,0,0,0,1},
-									 {1,0,0,0,0,0,0,0,0,1},
-									 {1,0,0,0,0,0,0,0,0,1},
-								     {1,0,0,0,0,0,0,0,0,1}, 
-									 {1,0,0,0,0,0,0,0,0,1}, 
-									 {1,0,0,0,0,0,0,0,0,1},
-									 {1,0,0,0,0,0,0,0,0,1},
-									 {1,1,1,1,1,1,1,1,1,1}};
+	public static void main(String[] args)
 	{
-		int[][] boardArray = new int [10][10];
+	 byte gridLayout[][]  =  {{1,1,1,1,1,1,1,1,1,1,1}, 
+							  {1,0,0,0,0,0,0,0,0,0,1}, 
+							  {1,0,0,0,0,0,0,0,0,0,1},
+							  {1,0,0,0,0,0,0,0,0,0,1},
+							  {1,0,0,0,0,0,0,0,0,0,1},
+							  {1,0,0,0,0,0,0,0,0,0,1},
+							  {1,0,0,0,0,0,0,0,0,0,1},
+							  {1,1,1,1,1,1,1,1,1,1,1}};
+	
+		int[][] boardArray = new int [11][8];
 		
+		
+		/*
 		for (int xPos = 0; xPos < 10; xPos++)
 		{
 			
 		    for (int yPos = 0; yPos < 10; yPos++)
 		    {
 		    	if(xPos == 0 && yPos <= 9)
-		    		System.out.print("x");
+		    		System.out.print("a");
 		    	
 		    	if(xPos == 9 && yPos <= 9)
-		    		System.out.print("x");
+		    		System.out.print("b");
 		    	
 		    	if(yPos == 0 && yPos <= 9)
-		    		System.out.print("x");		    	
+		    		System.out.print("c");
+		    	
+		    	if(yPos == 9 && yPos <= 9)
+		    		System.out.print("d");
 		    }   
 		    
 		    System.out.println("");
 		}
+		*/
+		
 	}
 	
 	public static JPanel getBoard()
