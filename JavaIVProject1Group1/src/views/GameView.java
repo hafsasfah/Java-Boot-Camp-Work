4e3ef0a47aee934be96130fe25a77b30773ea24a
ci6437 <dma_0000@msn.com>
2015-05-28 08:59:50 -0400

    Working on GameView and GameController

--- JavaIVProject1Group1/src/views/GameView.java
+++ JavaIVProject1Group1/src/views/GameView.java
@@ -1,20 +1,71 @@
 package views;
 
-import java.util.Observable;
-
-import controllers.GameController;
-import models.aGame;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import controllers.*;
+import models.*;
 
 public class GameView implements iGameView {
 
-	public GameView(aGame game, GameController gameController) {
-		// TODO Auto-generated constructor stub
+	private aGame game;
+	private iGameController gameController;
+	
+	private JPanel gamePanel;
+	
+	private JPanel currentPlayerPanel;
+	private JLabel titleLabel;
+	private JLabel currentPlayerLabel;
+	private JButton rollButton;
+	
+	private JPanel allPlayersPanel;
+	
+	public GameView( aGame game, GameController gameController )
+	{
+		this.game = game;
+		this.gameController = gameController;
+		
+		gamePanel = new JPanel();
+		gamePanel.setLayout( new BorderLayout() );
+		
+		currentPlayerPanel = new JPanel();
+		titleLabel = new JLabel( game.getName() );
+		currentPlayerLabel = new JLabel( game.getCurrentPlayer().getName() );
+		rollButton = new JButton("Roll");
+		rollButton.addActionListener( new RollButtonListener() );
+		currentPlayerPanel.add( titleLabel );
+		currentPlayerPanel.add( currentPlayerLabel );
+		currentPlayerPanel.add( rollButton );
+		
+		gamePanel.add( currentPlayerPanel, BorderLayout.NORTH );
+		
+		allPlayersPanel = new JPanel();
+		allPlayersPanel.setLayout( new GridLayout( game.getPlayers().size(), 1));
+		for ( Object player : game.getPlayers().toArray( ) )
+		{
+			allPlayersPanel.add( new PlayerView( (aPlayer) player ).getPlayerPanel() );
+		}
+		
+		gamePanel.add( allPlayersPanel, BorderLayout.WEST );
+		
+	}
+	
+	@Override
+	public JPanel getGameView() {
+		return gamePanel;
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
+		currentPlayerLabel.setText( game.getCurrentPlayer().getName() );
 	}
-
+	
+	private class RollButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			gameController.nextPlayerRolls();
+		}
+	}
 }
