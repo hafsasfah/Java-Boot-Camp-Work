58204d7a0ac577c4198b9d5d1d0ec212abeaad51
brandonctaylor <brandonctaylor@me.com>
2015-05-28 14:29:31 -0400

    Updated Game Repo Class

--- JavaIVProject1Group4/src/repo/GameRepo.java
+++ JavaIVProject1Group4/src/repo/GameRepo.java
@@ -2,22 +2,6 @@ package repo;
 import java.sql.*;
 public class GameRepo {
 	
-	public static void main(String [] args){
-		
-		
-		
-		
-		
-		}
-	/*
-	 * jdbc:postgresql://localhost/"Monopoly";
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 */
+	
 
 }
