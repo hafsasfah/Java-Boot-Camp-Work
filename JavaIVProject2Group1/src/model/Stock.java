9e56d1cdab0d1226d668746b619e9dd843f6c90f
CColon1 <sikz26300@gmail.com>
2015-06-01 17:02:47 -0400

    StockRepo

--- JavaIVProject2Group1/src/model/Stock.java
+++ JavaIVProject2Group1/src/model/Stock.java
@@ -2,8 +2,22 @@ package model;
 
 public class Stock 
 {
-
+	String ticker;
 	String name;
 	double openingPrice;
+	public Stock(String ticker, String name){
+		this.name = name;
+		this.ticker = ticker;
+	}
+	public String getname(){
+		return name;}
+	public String getTicker(){
+		return ticker;}
+	
+	public void setPrice(double price){
+		this.openingPrice=price;
+	}
+	public double getPrice(){
+		return openingPrice;}
 	
 }
---
+++ JavaIVProject2Group1/src/repo/StockRepo.java
@@ -0,0 +1,68 @@
+package repo;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import model.Stock;
+
+
+public class StockRepo {
+ArrayList<Stock> stocksList;
+
+
+	public static void main(String[] args){
+	
+		StockRepo sr = new StockRepo();
+	}
+
+
+
+	public StockRepo(){
+		stocksList = new ArrayList<Stock>();
+		buildStockList();
+	}
+	public ArrayList<Stock> getStockList(){
+		return stocksList;
+	}
+	public void buildStockList(){
+			Connection  connection;
+			
+			
+			// String url = "jdbc:postgresql://localhost/"+JOptionPane.showInputDialog("Enter Database to read from ");
+		  //   String user = JOptionPane.showInputDialog("Enter Username for "+ url);
+		   //  String password = JOptionPane.showInputDialog("Enter Database password for user \""+user+"\"");
+			 
+		     
+			 String url = "jdbc:postgresql://localhost/stock market";
+		     String user = "postgres";
+		     String password = "password";
+			 ResultSet rs;
+			  try 
+		        {
+		        	connection = DriverManager.getConnection(url, user, password);
+		        
+			        Statement db = connection.createStatement();
+			        rs = db.executeQuery("Select \"ticker\",\"name\"from \"stocks\"");
+			        while(rs.next()){  	
+			        	String ticker = rs.getString(1);
+			        	String name = rs.getString(2);
+			        	stocksList.add(new Stock(ticker,name));
+			        	System.out.println(rs.getString(1)+ " : "+rs.getString(2));
+			        	
+			        }
+			        
+		        }
+		        catch(SQLException e)
+		        {
+		        	e.printStackTrace();
+		        	System.err.println("Got an exception! "); 
+		        }
+			
+			
+			
+		}
+	
+	
+}
