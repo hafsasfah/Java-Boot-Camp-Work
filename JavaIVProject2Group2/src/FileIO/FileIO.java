13038517848aa976b3cb68e275127f1399546a3a
sgargesh <sachin.gargesh@gmail.com>
2015-06-01 13:06:08 -0400

    File Reader working. NOT FINAL

--- JavaIVProject2Group2/src/FileIO/FileIO.java
+++ JavaIVProject2Group2/src/FileIO/FileIO.java
@@ -1,5 +1,50 @@
 package FileIO;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
 public class FileIO {
-
+ 
+  public static void main(String[] args) {
+ 
+	FileIO obj = new FileIO();
+	obj.run();
+ 
+  }
+ 
+  public void run() {
+ 
+	BufferedReader br = null;
+	String line = "";
+	String cvsSplitBy = ",";
+ 
+	try {
+ 
+		br = new BufferedReader(new FileReader(csvFile));
+		while ((line = br.readLine()) != null) {
+ 
+		        // use comma as separator
+ 
+ 
+		}
+ 
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	} catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		if (br != null) {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+ 
+	System.out.println("Done");
+  }
+ 
 }
