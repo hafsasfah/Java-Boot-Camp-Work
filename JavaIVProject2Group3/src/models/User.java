4af9c5f75bb63df11dbf7c5ef46f3c1412d5c0ad
brandonctaylor <brandonctaylor@me.com>
2015-06-02 00:03:02 -0400

    Updated user model class.

--- JavaIVProject2Group3/src/models/User.java
+++ JavaIVProject2Group3/src/models/User.java
@@ -1,15 +1,18 @@
 package models;
 
+import java.util.Scanner;
+
 public class User {
 
 	private String userName;
 	private int userID;
 	private double cash;
 	
-	public User()
+	public User(String userName, int userID)
 	{
-		
-		
+		this.userName = userName;
+		this.userID = userID;
+		cash = 50000;
 	} 
 	
 	public String getUserName()
@@ -19,28 +22,28 @@ public class User {
 	
 	public void setUserName(String userName)
 	{
-		
+		Scanner input = new Scanner(System.in);
+		userName = input.nextLine();
+		input.close();
 	}
 	
 	public int getUserId()
 	{
 		return userID;
-		
 	}
 	
 	public void setUserId(int userId)
 	{
-		
+		userID = ;
 	}
 	
 	public double getCash()
 	{
 		return cash;
-		
 	}
 	
-	public void setCash()
+	public void setCash(double profit)
 	{
-		
+		cash += profit;
 	}
 }
