fc1425be6735b96b6cc38c7ad983981be4ea2d25
dglenn248 <dglenn55@hotmail.com>
2015-06-02 10:53:12 -0400

    Revert "Big Update"
    This reverts commit b41eb644ed4c87d8799702ed5860e78d88f3b950.

--- JavaIVProject2Group2/src/Models/Player.java
+++ JavaIVProject2Group2/src/Models/Player.java
@@ -1,48 +1,5 @@
 package Models;
 
-import java.util.Scanner;
 public class Player {
 
-	private String userName;
-	private int userID;
-	private double cash;
- 
-	public Player(String userName, int userID)
-	{
-	this.userName = userName;
-	this.userID = userID;
-	cash = 50000;
-	}
-
-	public String getUserName()
-	{
-	return userName;
-	}
-
-	public void setUserName(String userName)
-	{
-	Scanner input = new Scanner(System.in);
-	userName = input.nextLine();
-	input.close();
-	}
-
-	public int getUserId()
-	{
-	return userID;
-	}
-
-	public void setUserId(int userId)
-	{
-	this.userID = userID;
-	}
-
-	public double getCash()
-	{
-	return cash;
-	}
-
-	public void setCash(double profit)
-	{
-	cash += profit;
-	}
 }
