bcf45a2c5bc5a7aa98f266df8516664c7491f9d4
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-02 08:04:20 -0700

    updated
    updated personRepo class

--- JavaIVProject2Group1/src/repo/PersonRepo.java
+++ JavaIVProject2Group1/src/repo/PersonRepo.java
@@ -1,5 +1,35 @@
 package repo;
 
-public class PersonRepo {
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
+import javax.swing.JOptionPane;
+
+import model.Person;
+
+
+public class PersonRepo {
+	ArrayList<Person> personList;
+
+
+	public static void main(String[] args){
+	
+		PersonRepo pr = new PersonRepo();
+	}
+
+	public PersonRepo(){
+		personList = new ArrayList<Person>();
+		buildPersonList();
+	}
+	
+	public ArrayList<Person> getPersonList(){
+		return personList;
+	}
+	
+	public void buildPersonList(){
+			Connection  connection;
+	}
 }
