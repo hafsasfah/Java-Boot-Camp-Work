503e9861fcbb13a58fab700084fe63af9fd1179e
Eric Charnesky <eric.charnesky@careevolution.com>
2015-06-02 11:52:44 -0400

    Add OpeningPrice repo and reader

--- JavaIVProject2-Eric/src/data/OpeningPriceRepository.java
+++ JavaIVProject2-Eric/src/data/OpeningPriceRepository.java
@@ -1,5 +1,33 @@
 package data;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import models.OpeningPrice;
+
 public class OpeningPriceRepository {
 
+	private Connection connection;
+
+	public OpeningPriceRepository(Connection connection) {
+		this.connection = connection;
+	}
+
+	public boolean create(OpeningPrice openingPrice) 
+	{
+		try {
+			Statement statement = connection.createStatement();
+			String insertSQL = String.format("insert into \"OpeningPrice\" ( \"Ticker\", \"Date\", \"Price\" ) values ( '%s', '%s', %.2f )", 
+					openingPrice.getTicker(), openingPrice.getDate(), openingPrice.getPrice() );
+			statement.executeUpdate(insertSQL);
+			
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	
 }
