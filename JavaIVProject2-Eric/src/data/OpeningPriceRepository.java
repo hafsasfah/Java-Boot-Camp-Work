c67569aad89afb1cd21df83ce26b4dd2895d7a75
Eric Charnesky <eric.charnesky@careevolution.com>
2015-06-02 13:50:06 -0400

    OpeningPriceServlet add specific ticker

--- JavaIVProject2-Eric/src/data/OpeningPriceRepository.java
+++ JavaIVProject2-Eric/src/data/OpeningPriceRepository.java
@@ -1,10 +1,16 @@
 package data;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import models.OpeningPrice;
+import models.Stock;
 
 public class OpeningPriceRepository {
 
@@ -28,6 +34,52 @@ public class OpeningPriceRepository {
 		}
 		return false;
 		
+	}
+	
+	public List<OpeningPrice> get()
+	{
+		List<OpeningPrice> openingPrices = new ArrayList<OpeningPrice>();
+		
+		try {
+		
+		Statement statement = connection.createStatement();
+		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"OpeningPrice\"" );
+		ResultSet results = statement.executeQuery(query);
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		while ( results.next() )
+		{
+			openingPrices.add( new OpeningPrice( results.getString(1), formatter.format(results.getDate(2)), results.getDouble(3) ) );
+		}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return openingPrices;
+	}
+	
+	public List<OpeningPrice> get( String tickerName )
+	{
+		List<OpeningPrice> openingPrices = new ArrayList<OpeningPrice>();
+		
+		try {
+		
+		Statement statement = connection.createStatement();
+		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"OpeningPrice\" where \"Ticker\" = '%s'", tickerName );
+		ResultSet results = statement.executeQuery(query);
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		while ( results.next() )
+		{
+			openingPrices.add( new OpeningPrice( results.getString(1), formatter.format(results.getDate(2)), results.getDouble(3) ) );
+		}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return openingPrices;
 	}
 	
 }
--- JavaIVProject2-Eric/src/web/OpeningPriceServlet.java
+++ JavaIVProject2-Eric/src/web/OpeningPriceServlet.java
@@ -2,24 +2,64 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.OpeningPrice;
 import data.*;
 
 public class OpeningPriceServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
-		//OpeningPriceRepository openingPriceRepository = new OpeningPriceRepository( StockConnectionProvider.createConnection() );
+		OpeningPriceRepository openingPriceRepository = new OpeningPriceRepository( StockConnectionProvider.createConnection() );
     	
     	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println( ServletHelper.createHead( "OpeningPrice" ) );
+        
+        String requestedTicker = request.getParameter( "ticker" );
+
+        List<OpeningPrice> prices = new ArrayList<OpeningPrice>();
+        
+        if ( requestedTicker == null )
+        {
+        	prices.addAll(openingPriceRepository.get());
+        }
+        else
+        {
+        	prices.addAll(openingPriceRepository.get(requestedTicker));
+        }
+        
+        buildOpeningPriceTable( prices );
+        
+        out.println("</body>");
         out.println("</html>");
     }
+	
+	private String buildOpeningPriceTable( List<OpeningPrice> prices )
+	{
+		StringBuilder output = new StringBuilder();
+		
+		output.append("<table border='1'>");
+		output.append( "<tr><th>Ticker</th><th>Date</th><th>Price</th></tr>" );
+        
+        for ( OpeningPrice price : prices )
+        {
+        	output.append( "<tr>" );
+        	output.append( "<td>" + price.getTicker() + "</td>" );
+        	output.append( "<td>" + price.getDate() + "</td>" );
+        	output.append( "<td>" + price.getPrice() + "</td>" );
+        	output.append( "</tr>" );
+        }
+        output.append("</table>");
+        
+        return output.toString();
+	}
 	
 }
