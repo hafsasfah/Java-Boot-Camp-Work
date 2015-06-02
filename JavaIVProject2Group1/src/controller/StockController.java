78daa4a37e0e5608fc2017b70a5698cb5e5d0eea
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-02 06:23:39 -0700

    Update
    Created Interfaces for Stock,Person,Transaction
    
    Update the controllerClass

--- JavaIVProject2Group1/src/controller/StockController.java
+++ JavaIVProject2Group1/src/controller/StockController.java
@@ -10,16 +10,22 @@ public class StockController implements IStockController{
 	 ArrayList<Stock> Stock; 
 	 ArrayList<Transactions> Transaction;
 	 ArrayList<Person> Person;
+	 
 	 StockRepo sr;
+	 TransactionRepo tr;
+	 PersonRepo pr;
 
 	
 public void stockController()
 {
 	sr = new StockRepo();
 	this.Stock = sr.getStockList();
-	this.Transaction= new ArrayList<Transactions>();
-	this.Person = new ArrayList<Person>();
 	
+	tr= new TransactionRepo();
+	this.Transaction= tr.getTransactionList();
+	
+	pr= new PersonRepo();
+	this.Person = pr.getPersonRepoList();
 }
 
 
@@ -27,30 +33,20 @@ public void stockController()
 public ArrayList<Stock> viewStock() {
 	
 	return this.Stock;
-	// TODO Auto-generated method stub
-	
+	// TODO Auto-generated method stub	
 }
 
 
 @Override
 public ArrayList<Person> viewPerson() {
 	return this.Person;
-	// TODO Auto-generated method stub
-	
+	// TODO Auto-generated method stub	
 }
-
-
-
-
-
 
 @Override
 public ArrayList<Transactions> viewTransaction() {
 	// TODO Auto-generated method stub
 	return Transaction;
 }
-
-
-
 
 }
