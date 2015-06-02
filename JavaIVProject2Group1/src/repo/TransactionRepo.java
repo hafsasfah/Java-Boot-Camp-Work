acaaaaa5cb92d3d08c7cb3ba4da97fd6898f4367
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-02 07:54:59 -0700

    Updated
    update transactionRepo class

--- JavaIVProject2Group1/src/repo/TransactionRepo.java
+++ JavaIVProject2Group1/src/repo/TransactionRepo.java
@@ -1,5 +1,33 @@
 package repo;
 
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+import model.Transactions;
+
 public class TransactionRepo {
 
+	ArrayList<Transactions> transactionList;
+	
+	public static void main(String[] args){
+		
+			TransactionRepo tr = new TransactionRepo();
+		}
+
+	public TransactionRepo(){
+			transactionList = new ArrayList<Transactions>();
+			buildTransactionList();
+		}
+	
+	public ArrayList<Transactions> getTransactionList(){
+			return transactionList;
+		}
+	
+	public void buildTransactionList(){
+				Connection  connection;
+		}
 }
