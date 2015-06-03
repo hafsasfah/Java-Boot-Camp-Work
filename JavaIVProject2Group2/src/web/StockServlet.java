c931cc7ac3f7bae4928efae50951df984dfdfe56
dglenn248 <dglenn55@hotmail.com>
2015-06-03 11:55:16 -0400

    Working on StockServlet

--- JavaIVProject2Group2/src/web/StockServlet.java
+++ JavaIVProject2Group2/src/web/StockServlet.java
@@ -2,11 +2,15 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Models.Stock;
+import Repository.StockRepository;
 
 public class StockServlet extends HttpServlet 
 {
@@ -14,15 +18,25 @@ public class StockServlet extends HttpServlet
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws IOException, ServletException
     {
+    	
+    	StockRepository stockRepository = new StockRepository( StockConnectionProvider.createConnection() );
+    	
+    	
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<html>");
+        out.println(ServletHelper.createHead( "Stock" ) );
         out.println("<head>");
-        out.println("<title>Hello World!</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
         out.println("</body>");
         out.println("</html>");
+        
+        String pathInfo = request.getPathInfo();
+        
+        if ( pathInfo == null || pathInfo.equals("/") )
+        {
+	        out.println("<h1>S&P 500 Stocks</h1>");
+	        out.println("<table border='1'>");
+	        List<Stock> stocks = stockRepository.get();
     }
 }
