7949d1b286759080a12b65dabb2e98559426a1b5
Eric Charnesky <eric.charnesky@careevolution.com>
2015-06-04 12:07:23 -0400

    Player model repository and servlet

---
+++ JavaIVProject2-Eric/src/models/Player.java
@@ -0,0 +1,29 @@
+package models;
+
+public class Player 
+{
+	private String name;
+	private double money;
+
+	public Player( String name, double money )
+	{
+		this.setName(name);
+		this.setMoney(money);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMoney() {
+		return money;
+	}
+
+	public void setMoney(double money) {
+		this.money = money;
+	}
+}
