5b4c1d7a4e8edc1c68133b350be91be9e8d3b2da
plake06 <Pbl8806@mail.com>
2015-06-04 11:09:16 -0400

    almost there

--- JavaIVProject2Group3/src/web/StocksServlet.java
+++ JavaIVProject2Group3/src/web/StocksServlet.java
@@ -7,49 +7,60 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import models.*;
 import databases.*;
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import models.*;
+import databases.*;
 
 public class StocksServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-    		throws IOException, ServletException {
-        StocksTable stockRepository = new StocksTable();
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+    	StocksTable stocksTable = new StocksTable();
             	
     	response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println(ServletHelper.createHead("Stock"));
+        out.println( ServletHelper.createHead( "Stock" ) );
         out.println("<body>");
         
         String pathInfo = request.getPathInfo();
         
-        if (pathInfo == null || pathInfo.equals("/")) {
+        if ( pathInfo == null || pathInfo.equals("/") )
+        {
 	        out.println("<h1>S&P 500 Stocks</h1>");
 	        out.println("<table border='1'>");
-	        
-	        List<Stocks> stocks = stockRepository.get();
-	        
-	        if (stocks.size() > 0) {
+	        List<Stocks> stocks = stocksTable.get();
+	        if ( stocks.size() > 0 )
+	        {
 	        	out.println( "<tr><th>Ticker</th><th>Name</th></tr>" );
 	        }
-	        
-	        for (Stocks stock : stocks) {
-	        	out.println("<tr>");
-	        	out.println("<td>" + stock.getTicker() + "</td>");
-	        	out.println("<td>" + stock.getStockName() + "</td>");
-	        	out.println("</tr>");
+	        for ( Stocks stock : stocks )
+	        {
+	        	out.println( "<tr>" );
+	        	out.println( "<td>" + stock.getTicker() + "</td>" );
+	        	out.println( "<td>" + stock.getStockName() + "</td>" );
+	        	out.println( "</tr>" );
 	        }
 	        
 	        out.println("</table>");
         }
-        
-        else {
-        	String requestedTicker = pathInfo.substring(pathInfo.indexOf('/') + 1);
+        else
+        {
+        	String requestedTicker = pathInfo.substring( pathInfo.indexOf('/') + 1);
         	
-        	Stocks stock = stockRepository.get(requestedTicker) ;
-        	if (stock == null) {
-        		out.println(requestedTicker + " was not found");
+        	Stocks stock = stocksTable.get(requestedTicker) ;
+        	if ( stock == null )
+        	{
+        		out.println( requestedTicker + " was not found" );
         	}
-        	else {
-        		out.println(stock.getTicker() + " " + stock.getStockName());
+        	else
+        	{
+        		out.println( stock.getTicker() + " " + stock.getStockName() );
         	}
         }
 	    out.println("</body>");
