956279a49a585ebb3143ee427fae40c18fa2ffd6
Tommiec21 <Tommiec22@gmail.com>
2015-06-04 12:24:44 -0400

    Transaction Servlet Again.

--- JavaIVProject2Group1/src/model/Transactions.java
+++ JavaIVProject2Group1/src/model/Transactions.java
@@ -5,6 +5,46 @@ public class Transactions
 	
 	
 	Person buyer;
+	public Person getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(Person buyer) {
+		this.buyer = buyer;
+	}
+
+	public Stock getStock() {
+		return stock;
+	}
+
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+
+	public double getStockPrice() {
+		return stockPrice;
+	}
+
+	public void setStockPrice(double stockPrice) {
+		this.stockPrice = stockPrice;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getAmountBought() {
+		return amountBought;
+	}
+
+	public void setAmountBought(int amountBought) {
+		this.amountBought = amountBought;
+	}
+
 	Stock stock;
 	double stockPrice;
 	String date;
