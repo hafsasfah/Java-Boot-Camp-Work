3757e17972820170c2d677dcb94e1b726c43a371
sgargesh <sachin.gargesh@gmail.com>
2015-06-04 13:53:00 -0400

    Fix merge issues

--- JavaIVProject2Group2/src/FileIO/ImportTickerAndStockDate.java
+++ JavaIVProject2Group2/src/FileIO/ImportTickerAndStockDate.java
@@ -1,25 +1,67 @@
 package FileIO;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-public class ImportTickerAndStockDate {
+import Models.OpeningStock;
+import Repository.OpeningStockRepository;
+import Repository.StockConnectionProvider;
+
+public class ImportTickerAndStockDate 
+
+{
+
+	public static void main(String[] args)
+	{
+			new ImportTickerAndStockDate( "S&P500-2015-06-01.csv", "2015-06-01");
+	}
 	
-	public static void ImportTickerAndStockDate () throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File("s&p500-2015-06-01.csv"));
-		scanner.useDelimiter(",");
-		while(scanner.hasNext()){
-		System.out.print(scanner.next()+"|");
-		}
+private OpeningStockRepository openingPriceRepository;
+private String filename;
+private String date;
 
-		scanner.close();
+	public ImportTickerAndStockDate( String filename, String date )
+	{
+		this.filename = filename;
+		this.date = date;
+		openingPriceRepository = new OpeningStockRepository( StockConnectionProvider.createConnection() );
+		for ( OpeningStock openingPrice : read( ) )
+		{
+				openingPriceRepository.create( openingPrice );
+		}
+	}
 
-		}
-		
-		public static void main (String args[]) throws FileNotFoundException{
-			ImportTickerAndStockDate();
-		}
+
+public List<OpeningStock> read( )
+{
+	File stockFile = new File( filename );
+	Scanner scanner;
+	try
+{
+	scanner = new Scanner(stockFile);
+	List<OpeningStock> openingPrices = new ArrayList<OpeningStock>();
+	if ( scanner.hasNext() )
+{
+		String headerLineToIgnore = scanner.nextLine();
 }
-
-
+	while ( scanner.hasNext() )
+{
+	String line = scanner.nextLine();
+	int firstCommaIndex = line.indexOf(',');
+	String ticker = line.substring(0, firstCommaIndex );
+	double price = Double.parseDouble( line.substring(firstCommaIndex + 1) );
+	openingPrices.add( new OpeningStock( ticker, date, price) );
+}
+	scanner.close();
+	return openingPrices;
+} 
+	catch ( Exception e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+return null;
+}
+}
