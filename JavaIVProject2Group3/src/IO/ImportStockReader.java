0ad1454e29cd08b109fced7c1384029963519f4f
hafsasfah <hafsa.vahidy@gmail.com>
2015-06-04 10:25:47 -0400

    importstockreader stuff

--- JavaIVProject2Group3/src/IO/ImportStockReader.java
+++ JavaIVProject2Group3/src/IO/ImportStockReader.java
@@ -11,48 +11,49 @@ import repository.StockConnection;
 import databases.*;
 
 public class ImportStockReader {
-	public static void main(String []args){
-		new ImportStockReader("..\\S&P500Stocks.csv" );
-	}
-	private ConnectionHelper stockRepository;
 	private StocksTable stocksTable;
 	
+	public static void main(String []args) {
+		new ImportStockReader("S&P500Stocks.csv");
+	}
+	
 	public ImportStockReader( String filename ){
-		stockRepository= new ConnectionHelper();
-		stocksTable=new StocksTable();
-		for (Stocks stock: read( filename))
+		stocksTable = new StocksTable();
+		for (Stocks stock: read(filename))
 		{
 			stocksTable.create( stock );
 		}
 	}
 
-		public List<Stocks>read ( String filename1 ){
-			File filename = new File( filename1 );
-			Scanner scanner;
-			try 
+	public List<Stocks> read(String filename1) {
+		File filename = new File( filename1 );
+		Scanner scanner;
+		
+		try {
+			scanner = new Scanner(filename);
+			List<Stocks> stocks = new ArrayList<Stocks>();
+			
+			if ( scanner.hasNext() )
 			{
-				scanner = new Scanner(filename);
-				List<Stocks> stocks = new ArrayList<Stocks>();
-				if ( scanner.hasNext() )
-				{
-					String headerLineToIgnore = scanner.nextLine();
-				}
-				while ( scanner.hasNext() )
-				{
-					String line = scanner.nextLine();
-					int firstCommaIndex = line.indexOf(',');
-					String ticker = line.substring(0, firstCommaIndex );
-					String name = line.substring(firstCommaIndex + 1).replace("\"", "");
-					stocks.add( new Stocks(ticker, name) );
-				}
-				scanner.close();
-				return stocks;
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				String headerLineToIgnore = scanner.nextLine();
 			}
 			
-			return null;
+			while ( scanner.hasNext() )
+			{
+				String line = scanner.nextLine();
+				int firstCommaIndex = line.indexOf(',');
+				String ticker = line.substring(0, firstCommaIndex );
+				String name = line.substring(firstCommaIndex + 1).replace("\"", "");
+				stocks.add( new Stocks(ticker, name) );
+			}
+			scanner.close();
+			return stocks;
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+		
+		return null;
+	}
 		
 }
