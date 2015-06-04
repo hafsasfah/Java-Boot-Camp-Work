418c658d7431850e63c53a83871436998f2a2b3c
dglenn248 <dglenn55@hotmail.com>
2015-06-03 21:56:33 -0400

    Updating StockServlet

--- JavaIVProject2Group2/src/web/StockServlet.java
+++ JavaIVProject2Group2/src/web/StockServlet.java
@@ -38,5 +38,38 @@ public class StockServlet extends HttpServlet
 	        out.println("<h1>S&P 500 Stocks</h1>");
 	        out.println("<table border='1'>");
 	        List<Stock> stocks = stockRepository.get();
+	        
+	        if ( stocks.size() > 0 )
+	        {
+	        	out.println( "<tr><th>Ticker</th><th>Name</th></tr>" );
+	        }
+	        for ( Stock stock : stocks )
+	        {
+	        	out.println( "<tr>" );
+	        	out.println( "<td>" + stock.getTicker() + "</td>" );
+	        	out.println( "<td>" + stock.getName() + "</td>" );
+	        	out.println( "</tr>" );
+	        }
+	        
+	        out.println("</table>");
+        }
+        else
+        {
+        	String requestedTicker = pathInfo.substring( pathInfo.indexOf('/') + 1);
+        	
+        	Stock stock = stockRepository.get(requestedTicker) ;
+        	if ( stock == null )
+        	{
+        		out.println( requestedTicker + " was not found" );
+        	}
+        	else
+        	{
+        		out.println( stock.getTicker() + " " + stock.getName() );
+        	}
+        }
+	    out.println("</body>");
+        out.println("</html>");
+    }
+
     }
 }
