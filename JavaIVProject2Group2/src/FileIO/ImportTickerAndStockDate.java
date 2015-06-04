482cd34638356ec9ad1e8b8434592224adbfffa0
dglenn248 <dglenn55@hotmail.com>
2015-06-04 10:15:48 -0400

    Revert "Update on models and IO"
    This reverts commit 47332f1012ff02965390bb26a89ba31d91b6f153.

--- JavaIVProject2Group2/src/FileIO/ImportTickerAndStockDate.java
+++ JavaIVProject2Group2/src/FileIO/ImportTickerAndStockDate.java
@@ -1,62 +1,25 @@
 package FileIO;
 
-import java.io.*;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import Models.OpeningStock;
-import Repository.OpeningStockRepository;
-import Repository.StockConnectionProvider;
-public class ImportTickerandStockDate {
+public class ImportTickerAndStockDate {
+	
+	public static void ImportTickerAndStockDate () throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("s&p500-2015-06-01.csv"));
+		scanner.useDelimiter(",");
+		while(scanner.hasNext()){
+		System.out.print(scanner.next()+"|");
+		}
 
-	public static void main(String[] args)
-{
-		new ImportTickerandStockDate( "S&P500-2015-06-01.csv", "2015-06-01");
-}
-private OpeningStockRepository openingPriceRepository;
-private String filename;
-private String date;
+		scanner.close();
 
-public ImportTickerandStockDate( String filename, String date )
-{
-	this.filename = filename;
-	this.date = date;
-	openingPriceRepository = new OpeningStockRepository( StockConnectionProvider.createConnection() );
-	for ( OpeningStock openingPrice : read( ) )
-{
-		openingPriceRepository.create( openingPrice );
-}
+		}
+		
+		public static void main (String args[]) throws FileNotFoundException{
+			ImportTickerAndStockDate();
+		}
 }
 
-public List<OpeningStock> read( )
-{
-	File stockFile = new File( filename );
-	Scanner scanner;
-	try
-{
-	scanner = new Scanner(stockFile);
-	List<OpeningStock> openingPrices = new ArrayList<OpeningStock>();
-	if ( scanner.hasNext() )
-{
-		String headerLineToIgnore = scanner.nextLine();
-}
-	while ( scanner.hasNext() )
-{
-	String line = scanner.nextLine();
-	int firstCommaIndex = line.indexOf(',');
-	String ticker = line.substring(0, firstCommaIndex );
-	double price = Double.parseDouble( line.substring(firstCommaIndex + 1) );
-	openingPrices.add( new OpeningStock( ticker, date, price) );
-}
-	scanner.close();
-	return openingPrices;
-} 
-	catch ( Exception e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-}
-return null;
-}
-}
+
--- JavaIVProject2Group2/src/Models/OpeningStock.java
+++ JavaIVProject2Group2/src/Models/OpeningStock.java
@@ -1,47 +1,5 @@
 package Models;
 
+public class OpeningStock {
 
-public class OpeningStock
-	
-	{
-	private String ticker;
-	private double price;
-	private String date;
-	public OpeningStock( String ticker, String date, double price )
-	{
-
-		this.setTicker(ticker);
-
-		this.setPrice(price);
-	}
-	
-	public double getPrice() 
-	{
-	return price;
-	}
-
-	public void setPrice(double price)
-	{
-	this.price = price;
-	}
-
-	public String getTicker() 
-	{
-	return ticker;
-	}
-
-	public void setTicker(String ticker) 
-	{
-	this.ticker = ticker;
-	}
-	
-	public String getDate() 
-	{
-		return date;
-	}
-		
-	public void setDate(String date) 
-	{
-		this.date = date;
-	}
 }
