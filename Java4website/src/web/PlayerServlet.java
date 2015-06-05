0cbeb44bb4ca4aa0b1c376028f0c887f87807337
asjerniga <asjerniga@gmail.com>
2015-06-04 22:35:56 -0400

    PlayerServlet update

--- Java4website/src/web/PlayerServlet.java
+++ Java4website/src/web/PlayerServlet.java
@@ -1,9 +1,30 @@
 package web;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import PlayerRepo.PlayerRepo;
+import Stockrepo.StockConnectionProvider;
+
 public class PlayerServlet 
 {
 	
-	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException
+	{
+		
+		PlayerRepo playerRepository = new PlayerRepo(StockConnectionProvider.createConnection() );
+		
+		response.setContentType("text/html"); 
+		PrintWriter out = response.getWriter();
+		
+		
+		
+	}
 	
 	
 	
