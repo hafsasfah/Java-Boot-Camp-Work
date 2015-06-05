0a15a5c4d3004208b000e87519f909aeb3f9c994
asjerniga <asjerniga@gmail.com>
2015-06-04 20:48:39 -0400

    PlayerRepo updated 3

--- Java4website/src/PlayerRepo/PlayerRepo.java
+++ Java4website/src/PlayerRepo/PlayerRepo.java
@@ -15,27 +15,89 @@ public class PlayerRepo
 		
 	}
 	
-	public boolean createPlayer(Player player)
+	
+	public PlayerRepo()
+	{
+		
+		try
+		{
+			String url = "jdbc:postgresql://localhost/Stocks";
+			String username = "postgres";
+			String password = "dragon";
+			
+			connection = DriverManager.getConnection(url, username, password);
+			
+			
+		}
+		catch(SQLException E)
+		{
+			E.printStackTrace();
+			System.out.print("You messed up now.");
+		}
+		
+		
+	}
+	
+	
+	public void buildPlayerTable()
+	{
+		
+		try
+		{
+			
+			
+			Statement statement = connection.createStatement();
+			String buildPlayerTable = " CREATE TABLE \"Player\" " 
+										+"  \"Name\" character varying(100), " 
+										+ "  \"Cash\" double precision, " + ")"; 
+			
+			statement.executeQuery(buildPlayerTable);
+			
+			
+			
+		}catch(SQLException E)
+		{
+			
+			System.out.println("There is a problem building the player table");
+			E.printStackTrace();
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	public boolean buildPlayer(Player player)
 	{
 		try
 		{
 			Statement statement = connection.createStatement();
 			
-			String createPlayerInSQL = String.format("insert into \"Player\" (\"Name\" character varying(100) ", " \"Cash\" )  VALUES ( '%s', '%.2f')" , 
+			String createPlayerInSQL = String.format("insert into \"Player\" (\"Name\" ", " \"Cash\" )  VALUES ( '%s', '%.2f')" , 
 					player.getThePlayerName(), player.getThePlayerCash());  
 			
 			statement.executeQuery(createPlayerInSQL);
 			return true;
 			
-		}catch(Exception e)
+		}catch(SQLException E)
 		{
 			System.out.println("There is a problem in createPlayer!");
-			e.printStackTrace();
+			E.printStackTrace();
 			return false;
 		}
 		
 		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
