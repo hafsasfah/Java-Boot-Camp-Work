24fddda0bdf55b9259d001a174ee49d9043807a6
hafsasfah <hafsa.vahidy@gmail.com>
2015-06-05 02:01:08 -0400

    fixing

--- JavaIVProject2Group3/src/databases/StockPricesTable.java
+++ JavaIVProject2Group3/src/databases/StockPricesTable.java
@@ -57,93 +57,83 @@ public class StockPricesTable implements iStockPricesTable {
 	}
 
 	
-	public List<StockPrices> get()
-	{
+	public List<StockPrices> get() {
 		List<StockPrices> openingPrices = new ArrayList<StockPrices>();
 		
 		try {
-		
-		Statement statement = connection.createStatement();
-		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"StockPrices\"" );
-		ResultSet results = statement.executeQuery(query);
-		openingPrices.addAll( parseResults( results ) );
+			Statement statement = connection.createStatement();
+			String query = String.format("SELECT \"Ticker\", \"Date\", \"Price\" FROM \"StockPrices\"");
+			ResultSet results = statement.executeQuery(query);
+			openingPrices.addAll(parseResults(results));
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return openingPrices;
 	}
 	
-	public List<StockPrices> getByTicker( String tickerName )
-	{
+	public List<StockPrices> getByTicker(String tickerName) {
 		List<StockPrices> openingPrices = new ArrayList<StockPrices>();
 		
 		try {
-		
-		Statement statement = connection.createStatement();
-		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"StockPrices\" where \"Ticker\" = '%s'", tickerName );
-		ResultSet results = statement.executeQuery(query);
-		openingPrices.addAll( parseResults( results ) );
+			Statement statement = connection.createStatement();
+			String query = String.format("SELECT \"Ticker\", \"Date\", \"Price\" FROM \"StockPrices\" "
+					+ "WHERE \"Ticker\" = '%s'", tickerName);
+			ResultSet results = statement.executeQuery(query);
+			openingPrices.addAll(parseResults(results));
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return openingPrices;
 	}
 	
-	public List<StockPrices> getByDate( String date )
-	{
+	public List<StockPrices> getByDate(String date) {
 		List<StockPrices> openingPrices = new ArrayList<StockPrices>();
 		
 		try {
-		
-		Statement statement = connection.createStatement();
-		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"StockPrices\" where \"Date\" = '%s'", date );
-		ResultSet results = statement.executeQuery(query);
-		openingPrices.addAll( parseResults( results ) );
+			Statement statement = connection.createStatement();
+			String query = String.format("SELECT \"Ticker\", \"Date\", \"Price\" FROM \"StockPrices\" "
+					+ "WHERE \"Date\" = '%s'", date);
+			ResultSet results = statement.executeQuery(query);
+			openingPrices.addAll(parseResults(results));
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return openingPrices;
 	}
 	
-	public List<StockPrices> getByTickerAndDate( String ticker, String date )
-	{
+	public List<StockPrices> getByTickerAndDate(String ticker, String date) {
 		List<StockPrices> openingPrices = new ArrayList<StockPrices>();
 		
 		try {
-		
-		Statement statement = connection.createStatement();
-		String query = String.format("select \"Ticker\", \"Date\", \"Price\" from \"StockPrices\" where \"Ticker\" = '%s' and \"Date\" = '%s'", ticker, date );
-		ResultSet results = statement.executeQuery(query);
-		openingPrices.addAll( parseResults( results ) );
+			Statement statement = connection.createStatement();
+			String query = String.format("SELECT \"Ticker\", \"Date\", \"Price\" FROM \"StockPrices\" "
+					+ "WHERE \"Ticker\" = '%s' and \"Date\" = '%s'", ticker, date);
+			ResultSet results = statement.executeQuery(query);
+			openingPrices.addAll(parseResults(results));
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return openingPrices;
 	}
 	
-	private List<StockPrices> parseResults( ResultSet results )
-	{
+	private List<StockPrices> parseResults(ResultSet results) {
 		List<StockPrices> openingPrices = new ArrayList<StockPrices>();
 		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		
 		try {
-			while ( results.next() )
-			{
-				openingPrices.add( new StockPrices( results.getString(1), formatter.format(results.getDate(2)), results.getDouble(3) ) );
+			while (results.next()) {
+				openingPrices.add(new StockPrices(results.getString(1), 
+						formatter.format(results.getDate(2)), results.getDouble(3)));
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return openingPrices;
