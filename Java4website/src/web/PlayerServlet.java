cbfd09ec1b3e1e77dcf5dd999f638b18ef71ff6f
asjerniga <asjerniga@gmail.com>
2015-06-04 22:55:13 -0400

    PlayerServlet update

--- Java4website/src/web/PlayerServlet.java
+++ Java4website/src/web/PlayerServlet.java
@@ -2,11 +2,13 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import PlayerModel.PlayerModel;
 import PlayerRepo.PlayerRepo;
 import Stockrepo.StockConnectionProvider;
 
@@ -19,10 +21,45 @@ public class PlayerServlet
 		
 		PlayerRepo playerRepository = new PlayerRepo(StockConnectionProvider.createConnection() );
 		
-		response.setContentType("text/html"); 
-		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        out.println("<link rel=\"stylesheet\" href=\"../../bootstrap.min.css\" />");
+        out.println("</head>");
+        out.println("<body>");
+        
+        String pathInformation = request.getPathInfo();
 		
-		
+        if( pathInformation == null || pathInformation.equals("/"))
+        {
+        	
+        	out.println("<h1>Players</h1>");
+        	List<PlayerModel> players = playerRepository.getAllPlayers();
+        	
+        	if(players.size() > 0)
+        	{
+        		
+        		out.println("<tr><th>Name</th> <th>Cash</th></tr>");
+        		
+        	}
+        	
+        	for(PlayerModel player : players)
+        	{
+        		
+        		out.println( "<tr>" );
+	        	out.println( "<td>" + player.getThePlayerName()+ "</td>" );
+	        	out.println( "<td>" + String.format( "%.2f", player.getThePlayerCash()) + "</td>" );
+	        	out.println( "</tr>" );
+        		
+        	}
+        	
+        	out.println("</table>");
+        	
+        }
+        
+        
+        
+        
 		
 	}
 	
