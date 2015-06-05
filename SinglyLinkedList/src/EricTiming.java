ad3f8e75da7cf02327e6b65775d6f76f1026e5a7
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-05 06:02:45 -0700

    Updated Assignment #14

---
+++ SinglyLinkedList/src/EricTiming.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class EricTiming {
+
+	static int highestRandomNumber = 100000;
+	
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<Integer>();
+			
+		addRandomNumbersToList( "arrayList", arrayList, 1000 );
+		checkListsForContainsRandomNumber( "arrayList", arrayList, 100 );
+		addRandomNumbersToList( "linkedList", linkedList, 1000 );
+		checkListsForContainsRandomNumber( "linkedList", linkedList, 100 );
+		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 1000 );
+		checkListsForContainsRandomNumber( "singlyLinkedList", singlyLinkedList, 100 );
+		
+		addRandomNumbersToList( "arrayList", arrayList, 100000);
+		checkListsForContainsRandomNumber( "arrayList", arrayList, 1000 );
+		addRandomNumbersToList( "linkedList", linkedList, 10000000 );
+		checkListsForContainsRandomNumber( "linkedList", linkedList, 1000 );
+		addRandomNumbersToList( "singlyLinkedList", singlyLinkedList, 10000000 );
+		checkListsForContainsRandomNumber( "singlyLinkedList", singlyLinkedList, 1000 );
+	}
+	
+	public static void addRandomNumbersToList( String name, List<Integer> list, int numbersToAdd )
+	{
+		Random random = new Random(50);
+		long startTime, finishTime, elapsedTime;
+		startTime = System.nanoTime();
+		
+		for ( int count = 0; count < numbersToAdd; count++ )
+		{
+			list.add( random.nextInt(highestRandomNumber) );
+		}
+		
+		finishTime = System.nanoTime();
+		elapsedTime = finishTime - startTime;
+		
+		System.out.printf( "Adding %d to %s took %f millseconds\n\n", numbersToAdd, name, elapsedTime / 1000000.0 );
+	}
+	
+	public static void checkListsForContainsRandomNumber( String name, List<Integer> list, int numbersToCheck )
+	{
+		Random random = new Random(42);
+		long startTime, finishTime, elapsedTime;
+		startTime = System.nanoTime();
+		int matches = 0;
+		for ( int count = 0; count < numbersToCheck; count++ )
+		{
+			if ( list.contains( random.nextInt(highestRandomNumber) ) )
+			{
+				matches++;
+			}
+		}
+		
+		finishTime = System.nanoTime();
+		elapsedTime = finishTime - startTime;
+		
+		System.out.printf( "Checking %d to %s took %f millseconds and found %d matches\n\n", 
+				numbersToCheck, name, elapsedTime / 1000000.0, matches );
+	}
+	
+}
