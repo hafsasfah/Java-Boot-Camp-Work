3e5263b4ac1380aba32650bf09f7d72b14083d6d
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-05 07:34:17 -0700

    Adding TestCases
    Controller Class Test Cases

---
+++ JavaIVProject2Group1/src/controller/StockControllerTest.java
@@ -0,0 +1,39 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockControllerTest {
+
+	@Test
+	public void testGetStocks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStocksString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBuyer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuyStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSellStock() {
+		fail("Not yet implemented");
+	}
+
+}
