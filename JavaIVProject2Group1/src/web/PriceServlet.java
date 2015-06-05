2ac961751395753ae1b7e1bf28ae5521b9dc2564
Tommiec21 <Tommiec22@gmail.com>
2015-06-05 11:48:17 -0400

    update3

--- JavaIVProject2Group1/src/web/PriceServlet.java
+++ JavaIVProject2Group1/src/web/PriceServlet.java
@@ -36,14 +36,15 @@ public class PriceServlet extends HttpServlet
 			        List<Stock> stocks = stockRepository.getStockList();
 			        if ( stocks.size() > 0 )
 			        {
-			        	out.println( "<tr><th>Ticker</th><th>Name</th></tr>" );
+			        	out.println( "<tr><th>Ticker</th><th>Name</th><td>Price</td></tr>" );
 			        }
 			        for ( Stock stock : stocks )
 			        {
 			        	out.println( "<tr>" );
 			        	out.printf( "<td>" + stock.getTicker() + "</td>" );
-			        	out.printf( "<td>" + stock.getPrice() + "</td>");
 			        	out.printf( "<td>" + stock.getName() + "</td>" );
+			        	out.printf( "<td>" + stock.getPrice() + "</td>");
+			        	
 			        	out.println( "</tr>" );
 			        }
 			        
