1d2fdc697542a595cb1e71c4f6ad7afd15c67fa2
blaireblair <Blair@Blairs-MacBook-Air.local>
2015-06-07 08:29:54 -0700

    update

--- JavaIVProject1Group3/src/com/MonopolyGame/IPlayerRepo.java
+++
@@ -1,8 +0,0 @@
-package com.MonopolyGame;
-
-public interface IPlayerRepo {
-	boolean create(AbstractPlayer player);
-	AbstractPlayer get( int playerId);
-	boolean update(Abstract Player player);
-
-}
--- JavaIVProject1Group3/src/data/IPropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public interface IPropertyRepository {
-
-	boolean create( AbstractProperty property );
-	AbstractProperty get( int nameOfProperty );
-	boolean update( AbstractProperty property );
-	boolean delete( AbstractProperty property );
-}
